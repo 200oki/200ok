9bb5ac3503f37666728182e6d86f261c9761eb82
Changm-L <chang6585@naver.com>
2022-04-21 17:04:10 +0900

    modify: feat: add main page, v1

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,28 @@
+import './css/main.css'
+import React from 'react'
+import {useEffect, useState, useRef} from "react";
+import StyledItem from './components/StyledItem'
+import {useStyles} from "./utils/useStyles";
+import Player from 'react-audio-player';
+import mainAudio from './audio/Main_Theme_PG.mp3'
+
 function App() {
+    const classes = useStyles()
+    const [effect, setEffect] = useState('')
+    const ref = useRef(null)
+    useEffect(()=>{
+        setEffect('logo')
+        // document.querySelector('audio').play()
+    }, [])
+
   return (
-    <div className="App">
-      시작
-    </div>
+      <React.Fragment>
+          <React.Fragment>
+              <img className={effect} src='../public/images/main_logo_rm.png' alt={'animal Crossing'}/>
+              <StyledItem type='button' className={`${classes.startBtn} ${effect}`} content='시작하기' />
+              <Player className={classes.audio} src={mainAudio} autoPlay controls loop volume={0.3} controlsList={'nodownload'} ref={ref}/>
+          </React.Fragment>
+      </React.Fragment>
   );
 }
 
