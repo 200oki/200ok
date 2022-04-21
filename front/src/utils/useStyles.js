7aeaaa22bb11a88385adefdb98330471a2a2edc1
Changm-L <chang6585@naver.com>
2022-04-21 10:55:17 +0900

    feat: add ivory button css, white item css

--- front/src/utils/useStyles.js
+++ front/src/utils/useStyles.js
@@ -1,8 +1,45 @@
-import React from 'react'
 import {makeStyles} from "@mui/styles";
 
 export const useStyles = makeStyles({
-    foo:{
-        anything: anything
-    }
+    ivoryItem: {
+        border: "1.2px #c0dbdd solid",
+        color: 'black',
+        padding: '10px',
+        fontSize: '1.23rem',
+        lineHeight: '1.2em',
+        borderRadius: '25px',
+        textAlign: 'center',
+        cursor: 'pointer',
+        width: '25%',
+        height: '45px',
+        boxShadow: '1px 3px 2px 1px rgba(25, 25, 25, .2)',
+        backgroundColor: '#fff9e4',
+        fontFamily: 'TmoneyRoundWindExtraBold',
+        '&:hover':{
+            backgroundColor: '#cdcdcd',
+        },
+    },
+    whiteItem: {
+        border: "1.2px #f1f3f3 solid",
+        color: 'black',
+        padding: '10px',
+        fontSize: '1.23rem',
+        lineHeight: '1.2em',
+        borderRadius: '25px',
+        textAlign: 'center',
+        cursor: 'pointer',
+        width: '25%',
+        height: '45px',
+        boxShadow: '1px 3px 2px 1px rgba(25, 25, 25, .2)',
+        backgroundColor: '#f1f3f3',
+        fontFamily: 'TmoneyRoundWindExtraBold',
+        '&:hover':{
+            backgroundColor: '#cdcdcd',
+        },
+    },
+    startBtn: {
+        display: 'block !important',
+        margin: 'auto !important',
+        marginTop: '180px !important'
+    },
 })
