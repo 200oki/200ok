315497119cdef5778771499cf7a5106ef6f74a25
YunSeongJun <movomo@gmail.com>
2022-04-21 22:36:24 +0900

    modify: restore try catch

--- back/src/routers/scoreRouter.js
+++ back/src/routers/scoreRouter.js
@@ -7,41 +7,53 @@ const scoreRouter = Router();
 
 /*** Score will be added to db ***/
 scoreRouter.post("/scores", async function (req, res, next) {
-  const nickname = req.body.nickname;
-  const score = req.body.score;
-  const newScore = await ScoreService.addScore({
-    nickname,
-    score,
-  });
-  const body = {
-    success: true,
-    user: newScore,
-  };
+  try {
+    const nickname = req.body.nickname;
+    const score = req.body.score;
+    const newScore = await ScoreService.addScore({
+      nickname,
+      score,
+    });
+    const body = {
+      success: true,
+      user: newScore,
+    };
 
-  return res.status(status.STATUS_201_CREATED).json(body);
+    return res.status(status.STATUS_201_CREATED).json(body);
+  } catch (error) {
+    next(error);
+  }
 });
 
 /*** Get current user's rank and score ***/
 scoreRouter.get("/scores/:id", async function (req, res, next) {
-  const id = req.params.id;
-  const userRank = await ScoreService.getUserRank({ id });
-  const body = {
-    success: true,
-    user: userRank,
-  };
+  try {
+    const id = req.params.id;
+    const userRank = await ScoreService.getUserRank({ id });
+    const body = {
+      success: true,
+      user: userRank,
+    };
 
-  return res.status(200).json(body);
+    return res.status(200).json(body);
+  } catch (error) {
+    next(error);
+  }
 });
 
 /*** ranking ***/
 scoreRouter.get("/scorelist", async function (req, res, next) {
-  const rankList = await ScoreService.getRankList();
-  const body = {
-    success: true,
-    user: rankList,
-  };
+  try {
+    const rankList = await ScoreService.getRankList();
+    const body = {
+      success: true,
+      user: rankList,
+    };
 
-  return res.status(200).json(body);
+    return res.status(200).json(body);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export { scoreRouter };
