ae8145971e3320b1a3507b37cadaa0aa141c975c
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-04-22 02:09:51 +0900

    modify: rankList

--- back/src/db/models/Score.js
+++ back/src/db/models/Score.js
@@ -9,15 +9,41 @@ class Score {
   // 본인의 랭크 찾기
   // -1로 역순 정렬
   static async findUserRank({ id }) {
-    const userScore = await ScoreModel.findOne({ id });
-    return userScore;
+    const rankList = await ScoreModel.aggregate([
+      {
+        $setWindowFields: {
+          sortBy: { score: -1 },
+          output: {
+            rank: {
+              $rank: {}
+            }
+          }
+        }
+      }
+    ]);
+
+    const userRank = rankList.findIndex(i => i.id == id);
+    
+    return rankList[userRank];
   }
 
   static async findTopRank() {
     // top 몇 위까지 뽑아낼건지
     let top = 3;
-    const rankList = await ScoreModel.find().sort({ "score": -1 }).limit(top);
-    return rankList
+    const rankList = await ScoreModel.aggregate([
+      {
+        $setWindowFields: {
+          sortBy: { score: -1 },
+          output: {
+            rank: {
+              $rank: {}
+            }
+          }
+        }
+      }
+    ]);
+    
+    return rankList.slice(0, 3);
   }
 }
 
--- back/src/routers/scoreRouter.js
+++ back/src/routers/scoreRouter.js
@@ -1,59 +1,76 @@
 import is from "@sindresorhus/is";
 import { Router } from "express";
 import { ScoreService } from "../services/scoreService.js";
-import * as status from "../utils/status";
+import * as status from "../utils/status.js";
 
 const scoreRouter = Router();
 
 /*** Score will be added to db ***/
-scoreRouter.post("/scores", async function (req, res, next) {
+scoreRouter.post(
+  "/scores",
+  async function (req, res, next) {
   try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "필수 파라미터가 존재하지 않습니다."
+      );
+    }
+
     const nickname = req.body.nickname;
     const score = req.body.score;
+
     const newScore = await ScoreService.addScore({
       nickname,
       score,
     });
-    const body = {
-      success: true,
-      user: newScore,
-    };
 
-    return res.status(status.STATUS_201_CREATED).json(body);
-  } catch (error) {
-    next(error);
-  }
-});
+    if (newScore.errorMessage) {
+      throw new Error(newScore.errorMessage);
+    }
+
+      res.status(status.STATUS_201_CREATED).json(newScore);
+    } catch (err) {
+      next(err);
+    }
+  } 
+);
 
 /*** Get current user's rank and score ***/
-scoreRouter.get("/scores/:id", async function (req, res, next) {
-  try {
-    const id = req.params.id;
-    const userRank = await ScoreService.getUserRank({ id });
-    const body = {
-      success: true,
-      user: userRank,
-    };
-
-    return res.status(200).json(body);
-  } catch (error) {
-    next(error);
+scoreRouter.get(
+  "/scores/:id",
+  async function (req, res, next) {
+    try {
+      const id = req.params.id;
+      const userRank = await ScoreService.getUserRank({ id });
+  
+      if (userRank.errorMessage) {
+        throw new Error(userRank.errorMessage);
+      }
+  
+      res.status(status.STATUS_200_OK).send(userRank);
+    } catch (err) {
+      next(err);
+    }
   }
-});
-
+);
+    
 /*** ranking ***/
-scoreRouter.get("/scorelist", async function (req, res, next) {
-  try {
-    const rankList = await ScoreService.getRankList();
-    const body = {
-      success: true,
-      user: rankList,
-    };
-
-    return res.status(200).json(body);
-  } catch (error) {
-    next(error);
+scoreRouter.get(
+  "/scorelist",
+  async function (req, res, next) {
+    try {
+      const userRank = await ScoreService.getRankList();
+  
+      if (userRank.errorMessage) {
+        throw new Error(userRank.errorMessage);
+      }
+  
+      res.status(status.STATUS_200_OK).send(userRank);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
+
 
 export { scoreRouter };
