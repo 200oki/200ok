ebdfd7f738c3de2bbf3ad1271d2290f71bc40858
kminzy <kminzy0704@gmail.com>
2022-04-21 20:35:33 +0900

    modify: change App.js structure feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/App.js
+++ front/src/App.js
@@ -1,40 +1,26 @@
-import InputNickname from "./components/match/InputNickname";
 import styled from "./css/App.module.css";
-import Today from './components/Today/Today';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import './css/main.css'
-import React from 'react'
-import {useEffect, useState, useRef} from "react";
-import StyledItem from './components/StyledItem'
-import {useStyles} from "./utils/useStyles";
-import Player from 'react-audio-player';
-import mainAudio from './audio/Main_Theme_PG.mp3'
+import Main from "./components/Main";
+import Today from "./components/Today/Today";
+import InputNickname from "./components/match/InputNickname";
+
+import React from "react";
+import { useEffect, useState, useRef } from "react";
 
 function App() {
-    const today = new Date();
-    const classes = useStyles()
-    const [effect, setEffect] = useState('')
-    const ref = useRef(null)
-    useEffect(()=>{
-        setEffect('logo')
-        // document.querySelector('audio').play()
-    }, [])
+  const today = new Date();
 
   return (
     <div className={styled.App}>
-      {/* <InputNickname /> */}
       <Router>
         <Routes>
-          <Route path='/today' element={<Today today = {today}/>} />
+          <Route path="/" element={<Main />} />
+          {/* <Route path='/today' element={<Today today = {today}/>} /> */}
+          <Route path="/match" element={<InputNickname />} />
         </Routes>
       </Router>
-        <img className={effect} src='../public/images/main_logo_rm.png' alt={'animal Crossing'}/>
-        <StyledItem type='button' className={`${classes.startBtn} ${effect}`} content='시작하기' />
-        <Player className={classes.audio} src={mainAudio} autoPlay controls loop volume={0.3} controlsList={'nodownload'} ref={ref}/>
     </div>
-
-
   );
 }
 
---
+++ front/src/components/Main.js
@@ -0,0 +1,44 @@
+import "../css/main.css";
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import StyledItem from "../components/StyledItem";
+import { useStyles } from "../utils/useStyles";
+import Player from "react-audio-player";
+import mainAudio from "../audio/Main_Theme_PG.mp3";
+
+function Main() {
+  const classes = useStyles();
+  const [effect, setEffect] = useState("");
+  const ref = useRef(null);
+  useEffect(() => {
+    setEffect("logo");
+    // document.querySelector('audio').play()
+  }, []);
+
+  return (
+    <div className="mainWrapper">
+      <img
+        className={effect}
+        src="/public/images/main_logo_rm.png"
+        alt={"animal Crossing"}
+      />
+      <StyledItem
+        type="button"
+        className={`${classes.startBtn} ${effect}`}
+        content="시작하기"
+      />
+      <Player
+        className={classes.audio}
+        src={mainAudio}
+        autoPlay
+        controls
+        loop
+        volume={0.3}
+        controlsList={"nodownload"}
+        ref={ref}
+      />
+    </div>
+  );
+}
+
+export default Main;
