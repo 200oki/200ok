c882a7642e5628082c9888e87dd30c8bb0fc4932
HONG <loans93@gmail.com>
2022-04-21 19:24:48 +0900

    feat : 날짜, 생일인 주민 알림 문구 추가
    feat : 새로운 기능 추가
    fix : 버그 수정
    docs : 문서 수정
    modify : 기능 수정 및 코드 수정
    refact : 리팩토링

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect, useReducer, createContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Spinner } from 'react-bootstrap';
 import * as Api from './api';
-import { loginReducer, searchReducer } from './reducer';
 import Today from './components/Today/Today';
 
 export const UserStateContext = createContext(null);
