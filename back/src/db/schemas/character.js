9d1558ab816a7b424ffe20bb26b137aacbe4255f
YunSeongJun <movomo@gmail.com>
2022-04-22 11:12:38 +0900

    feat: add characters mock data

---
+++ back/src/db/schemas/character.js
@@ -0,0 +1,45 @@
+// import * as characters from "./characters.json";
+
+const mock = [
+  {
+    id: "admiral",
+    special: false,
+    name_en: "Admiral",
+    name_ko: "일섭",
+    image_icon: "https://acnhcdn.com/latest/NpcIcon/brd06.png",
+    image_photo: "https://acnhcdn.com/latest/NpcBromide/NpcNmlBrd06.png",
+    image_house:
+      "https://acnhcdn.com/drivesync/render/houses/brd06_39_Admiral.png",
+    species: "Bird",
+    gender: "Male",
+    gender_asia: "Male",
+    personality: "Cranky",
+    personality_subtype: "A",
+    hobby: "nature",
+    birthday: "01-27",
+    birthday_month: 1,
+    birthday_day: 27,
+    catchphrase: "aye aye",
+    favorite_song: "Steep Hill",
+    favorite_saying: "Only quitters give up.",
+    styles: ["Cool"],
+    colors: ["Black", "Blue"],
+  },
+  {
+    id: "cyrus",
+    special: true,
+    name_en: "Cyrus",
+    name_ko: "리포",
+    image_icon: "https://acnhcdn.com/latest/NpcIcon/alp.png",
+    image_photo: "https://acnhcdn.com/latest/NpcPoster/NpcSpAlp.png",
+    gender: "Male",
+    gender_asia: "Male",
+    birthday: "01-26",
+    birthday_month: 1,
+    birthday_day: 26,
+  },
+];
+
+const characters = mock;
+
+export { characters };
