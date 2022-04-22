b2da917893d2724f4e68fb8495485a4233bbc1d0
YunSeongJun <movomo@gmail.com>
2022-04-22 12:24:01 +0900

    feat: add CharacterService & blueprint

---
+++ back/src/services/characterService.js
@@ -0,0 +1,38 @@
+import { Character } from "../db";
+
+/**
+ * @typedef {{
+ *  errorMessage: string,
+ *  statusCode: number
+ * }} errorinfo
+ */
+
+/** 캐릭터 서비스 클래스입니다.
+ *
+ * 캐릭터 데이터는 읽기 전용입니다.
+ * 찾는 `id`가 없으면 `errorinfo`를 반환합니다.
+ */
+class CharacterService {
+  /** 캐릭터 한명을 골라 반환합니다. 찾는 `id`가 없으면 404 에러입니다.
+   *
+   * @arg {{string}} id - 캐릭터 `id`는 영문 이름의 소문자/공백제거/아스키 버전입니다.
+   * @return {{any}|errorinfo} character
+   */
+  static async get({ id }) {}
+
+  /** 생일이 일치하는 캐릭터의 배열을 반환합니다.
+   *
+   * @arg {{string}} birthday - `MM-DD` 포맷 날짜입니다.
+   * @return {{any}[]} characters - 캐릭터의 배열입니다.
+   * - 생일이 같은 캐릭터가 여러 명일 수도 있고, 없을 수도 있습니다.
+   */
+  static async getByBirthday({ id }) {}
+
+  /** 전체 캐릭터를 `{ id: name_ko }` 형식으로 반환합니다.
+   *
+   * @return {{any}} characterNames
+   */
+  static async list() {}
+}
+
+export { CharacterService };
