4ea923fa2a9aef0069eb943922444cbd1cc585ef
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-04-23 02:42:20 +0900

    refact: validator feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- back/src/routers/scoreRouter.js
+++ back/src/routers/scoreRouter.js
@@ -1,71 +1,93 @@
-import is from "@sindresorhus/is";
 import { Router } from "express";
 import { ScoreService } from "../services/scoreService.js";
+import { validate, notFoundValidate } from "../middlewares/validator.js";
+import { check, body } from "express-validator";
 import * as status from "../utils/status.js";
 
 const scoreRouter = Router();
 
 scoreRouter.post(
   "/scores",
+  [
+    body("nickname")
+      .exists()
+      .withMessage("닉네임을 입력해주세요.")
+      .bail(),
+    body("score")
+      .exists()
+      .withMessage("점수를 입력해주세요.")
+      .bail(),
+    validate,
+  ],
   async (req, res, next) => {
-  try {
-    if (is.emptyObject(req.body)) {
-      throw new Error(
-        "필수 파라미터가 존재하지 않습니다."
-      );
-    }
-
-    const nickname = req.body.nickname;
-    const score = req.body.score;
-
+    const { nickname, score } = req.body;
     const newScore = await ScoreService.addScore({
       nickname,
       score,
     });
 
-    if (newScore.errorMessage) {
-      throw new Error(newScore.errorMessage);
-    }
+    const body = {
+      success: true,
+    };
 
-      res.status(status.STATUS_201_CREATED).json(newScore);
-    } catch (err) {
-      next(err);
-    }
+    return res.status(status.STATUS_201_CREATED).json(body);
   } 
 );
 
 scoreRouter.get(
   "/scores/:id",
+  [
+    check("id")
+      .trim()
+      .isLength({ min: 1 })
+      .exists()
+      .withMessage("parameter 값으로 유저 아이디를 입력해주세요.")
+      .bail(),
+    notFoundValidate,
+    validate,
+  ],
   async (req, res, next) => {
-    try {
-      const id = req.params.id;
-      const userRank = await ScoreService.getUserRank({ id });
-  
-      if (userRank.errorMessage) {
-        throw new Error(userRank.errorMessage);
-      }
-  
-      res.status(status.STATUS_200_OK).send(userRank);
-    } catch (err) {
-      next(err);
+    const id = req.params.id;
+    const userRank = await ScoreService.getUserRank({ id });
+
+    if (userRank === undefined) {
+      const body = {
+        success: false,
+        detail: "id가 존재하지 않습니다."
+      };
+
+      return res.status(status.STATUS_400_BADREQUEST).json(body);
     }
+
+    const body = {
+      success: true,
+      userRank,
+    };
+
+    res.status(status.STATUS_200_OK).json(body);
   }
 );
     
 scoreRouter.get(
   "/scorelist",
   async (req, res, next) => {
-    try {
-      const userRank = await ScoreService.getRankList();
-  
-      if (userRank.errorMessage) {
-        throw new Error(userRank.errorMessage);
-      }
-  
-      res.status(status.STATUS_200_OK).send(userRank);
-    } catch (err) {
-      next(err);
+    const rankList = await ScoreService.getRankList();
+
+    if (rankList.length == 0) {
+      const body = {
+        success: false,
+        detail: "게임을 한 유저가 존재하지 않습니다."
+      };
+
+      return res.status(status.STATUS_400_BADREQUEST).json(body);
     }
+
+    const body = {
+      success: true,
+      rankList,
+    };
+
+    res.status(status.STATUS_200_OK).json(body);
   }
 );
 
