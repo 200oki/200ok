c3b9d778d45634cd5e13b60d197014c97dec4562
kminzy <kminzy0704@gmail.com>
2022-04-22 11:53:01 +0900

    modify: add page scroll to result page feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/components/match/MatchResult.js
+++ front/src/components/match/MatchResult.js
@@ -1,28 +1,109 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useEffect } from "react";
 import styled from "../../css/match.module.css";
 
 import { NicknameContext } from "../../context/NicknameContext";
 
+const DIVIDER_HEIGHT = 5;
+
 function MatchResult() {
-  const { nickname, setNickname } = useContext(NicknameContext);
+  const { nickname } = useContext(NicknameContext);
+  const outerDivRef = useRef();
+
+  const wheelHandler = (e) => {
+    e.preventDefault();
+
+    const { deltaY } = e;
+    const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
+    const pageHeight = window.innerHeight;
+
+    if (deltaY > 0) {
+      // 스크롤 내릴 때
+      if (scrollTop >= 0 && scrollTop < pageHeight) {
+        outerDivRef.current.scrollTo({
+          top: pageHeight + DIVIDER_HEIGHT,
+          left: 0,
+          behavior: "smooth",
+        });
+      } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+        outerDivRef.current.scrollTo({
+          top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+          left: 0,
+          behavior: "smooth",
+        });
+      } else {
+        outerDivRef.current.scrollTo({
+          top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+          left: 0,
+          behavior: "smooth",
+        });
+      }
+    } else {
+      // 스크롤 올릴 때
+      if (scrollTop >= 0 && scrollTop < pageHeight) {
+        outerDivRef.current.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
+      } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+        outerDivRef.current.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
+      } else {
+        outerDivRef.current.scrollTo({
+          top: pageHeight + DIVIDER_HEIGHT,
+          left: 0,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
+
+  const goToBottom = (e) => {
+    e.preventDefault();
+
+    const pageHeight = window.innerHeight;
+
+    outerDivRef.current.scrollTo({
+      top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const outerDivRefCurrent = outerDivRef.current;
+    outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+    return () => {
+      outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
+    };
+  }, []);
 
   return (
-    <div className={styled.Wrapper}>
-      <div className={styled.imgWrapper}>
-        <img src="/image/Aurora.png" />
+    <div className={styled.outer} ref={outerDivRef}>
+      <div className={styled.inner}>
+        <div className={styled.imgWrapper}>
+          <img src="/images/Aurora.png" />
+        </div>
+        <div className={styled.textWrapper}>
+          <div>{nickname} 님과 잘 어울리는 주민은</div>
+          <div className={styled.villagerName}>❝ 오로라 ❞</div>
+          <div>귀염뽀짝 어쩌구 저쩌구</div>
+          <div>구구절절 쫑알쫑알</div>
+          <div>최고의 궁합!</div>
+        </div>
+        <div className={styled.btnsWrapper}>
+          <button>공유하기</button>
+          <button>다시하기</button>
+          <button onClick={(e) => goToBottom(e)}>반응 남기기</button>
+        </div>
       </div>
-      <div className={styled.textWrapper}>
-        <div>{nickname} 님과 잘 어울리는 주민은</div>
-        <div className={styled.villagerName}>❝ 오로라 ❞</div>
-        <div>귀염뽀짝 어쩌구 저쩌구</div>
-        <div>구구절절 쫑알쫑알</div>
-        <div>최고의 궁합!</div>
-      </div>
-      <div className={styled.btnsWrapper}>
-        <button>공유하기</button>
-        <button>다시하기</button>
-        <button>반응 남기기</button>
-      </div>
+      <div className={styled.divider}></div>
+      <div className={styled.inner}>랭킹 영역</div>
+      <div className={styled.divider}></div>
+      <div className={styled.inner}>댓글 영역</div>
     </div>
   );
 }
