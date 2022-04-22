f2efeb1a682133f764872f5227aea2b0624b69e6
YunSeongJun <movomo@gmail.com>
2022-04-22 12:45:48 +0900

    docs: changed birthday party plan

--- back/src/services/characterService.js
+++ back/src/services/characterService.js
@@ -20,10 +20,10 @@ class CharacterService {
    */
   static async get({ id }) {}
 
-  /** 생일이 일치하는 캐릭터의 배열을 반환합니다.
+  /** 생일이 일치하는 캐릭터들의 객체를 반환합니다.
    *
    * @arg {{string}} birthday - `MM-DD` 포맷 날짜입니다.
-   * @return {{any}[]} characters - 캐릭터의 배열입니다.
+   * @return {{any}} characters - 캐릭터들의 객체입니다.
    * - 생일이 같은 캐릭터가 여러 명일 수도 있고, 없을 수도 있습니다.
    */
   static async getByBirthday({ id }) {}
