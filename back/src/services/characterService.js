9a884408a84d78a51d2999c5a84a088c121b5f9d
YunSeongJun <movomo@gmail.com>
2022-04-22 16:40:08 +0900

    modify: add fields filter to .get

--- back/src/services/characterService.js
+++ back/src/services/characterService.js
@@ -1,3 +1,5 @@
+import _ from "underscore";
+
 import { Character } from "../db/index.js";
 import * as status from "../utils/status.js";
 
@@ -16,13 +18,20 @@ import * as status from "../utils/status.js";
 class CharacterService {
   /** 캐릭터 한명을 골라 반환합니다. 찾는 `id`가 없으면 404 에러입니다.
    *
-   * @arg {{string}} id - 캐릭터 `id`는 영문 이름의 소문자/공백제거/아스키 버전입니다.
+   * @arg {{id: string, fields: string[]}} payload
+   * @arg {string} payload.id - `id`는 영문 이름의 소문자/공백제거/아스키 버전입니다.
+   * @arg {string[]} payload.fields - 포함하고 싶은 필드 목록입니다.
+   * - 빈 배열이면 모든 필드를 포함합니다.
    * @return {{any}|errorinfo} character
    */
-  static async get({ id }) {
+  static async get({ id, fields = [] }) {
     const character = await Character.get({ id });
     if (character) {
-      return character;
+      if (fields.length) {
+        return _(character).pick(fields);
+      } else {
+        return character;
+      }
     } else {
       return {
         errorMessage: `character record {${id}} not found`,
