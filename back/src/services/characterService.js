0e113a2222612264950fadfc038f71202ec8dd2b
YunSeongJun <movomo@gmail.com>
2022-04-22 13:21:40 +0900

    fix: typo

--- back/src/services/characterService.js
+++ back/src/services/characterService.js
@@ -37,7 +37,7 @@ class CharacterService {
    * @return {{any}} characters - 캐릭터들의 객체입니다.
    * - 생일이 같은 캐릭터가 여러 명일 수도 있고, 없을 수도 있습니다.
    */
-  static async getByBirthday({ id }) {}
+  static async getByBirthday({ birthday }) {}
 
   /** 전체 캐릭터를 `{ id: name_ko }` 형식으로 반환합니다.
    *
