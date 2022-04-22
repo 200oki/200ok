9928325d4cd4a8ec11ecf1f4b677ba6a664fe79a
YunSeongJun <movomo@gmail.com>
2022-04-22 17:04:31 +0900

    modify: add field type query to GET /:id

--- back/src/routers/characterRouter.js
+++ back/src/routers/characterRouter.js
@@ -6,14 +6,20 @@ import { RequestError } from "../utils/errors.js";
 
 const router = Router();
 
-/** query: birthday=mm-dd */
+/** query: birthday=mm-dd&fields=name_ko,name_en */
 router.get("/characters", async (req, res, next) => {
   try {
     let found;
     if (req.query.birthday) {
       // birthday 쿼리가 있을 때는 오늘의 생일 모드입니다.
+      // 쿼리에 원하는 필드값을 넣을 수 있습니다.
+      let fields = [];
+      if (req.query.fields) {
+        fields = req.query.fields.split(",");
+      }
       found = await CharacterService.getByBirthday({
         birthday: req.query.birthday,
+        fields,
       });
     } else {
       // 쿼리가 없으면 전체 캐릭터 이름 사전을 보내줍니다.
