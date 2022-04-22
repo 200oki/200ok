92464be207a191870634c13fd045604e2303fb6d
YunSeongJun <movomo@gmail.com>
2022-04-22 13:29:30 +0900

    feat: implement charRouter

--- back/src/routers/characterRouter.js
+++ back/src/routers/characterRouter.js
@@ -8,6 +8,17 @@ const router = Router();
 
 router.get("/characters", async (req, res, next) => {
   try {
+    if (req.query.birthday) {
+      // birthday 쿼리가 있을 때는 생일 검색 모드입니다.
+      const found = await CharacterService.getByBirthday({
+        birthday: req.query.birthday,
+      });
+      res.status(status.STATUS_200_OK).json({ success: true, payload: found });
+    } else {
+      // 쿼리가 없으면 전체 캐릭터 이름 사전을 보내줍니다.
+      const found = await CharacterService.list();
+      res.status(status.STATUS_200_OK).json({ success: true, payload: found });
+    }
   } catch (error) {
     next(error);
   }
@@ -15,6 +26,11 @@ router.get("/characters", async (req, res, next) => {
 
 router.get("/characters/:id", async (req, res, next) => {
   try {
+    const found = await CharacterService.get({ id: req.params.id });
+    if (found.errorMessage) {
+      throw new RequestError({ status: found.statusCode }, found.errorMessage);
+    }
+    res.status(status.STATUS_200_OK).json({ success: true, payload: found });
   } catch (error) {
     next(error);
   }
