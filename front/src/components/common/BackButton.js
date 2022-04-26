20d7360329f76d0c08eee78f720630882f6bfb22
Changm-L <chang6585@naver.com>
2022-04-27 00:04:29 +0900

    feat : add BackButton

---
+++ front/src/components/common/BackButton.js
@@ -0,0 +1,23 @@
+import { ArrowLeft } from "@mui/icons-material";
+import { Typography } from "@mui/material";
+import React from "react";
+import { useStyles } from "../../utils/useStyles";
+import { useNavigate } from "react-router-dom";
+
+const BackButton = () => {
+  const classes = useStyles();
+  const navigator = useNavigate();
+
+  const backHome = () => {
+    navigator("/explore");
+  };
+
+  return (
+    <Typography className={classes.quizName} onClick={backHome}>
+      <ArrowLeft className={classes.leftArrow} />
+      메인메뉴
+    </Typography>
+  );
+};
+
+export default BackButton;
