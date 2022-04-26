1e61cc3cc09e49525470f67974ac716426359610
hongloans <loans93@gmail.com>
2022-04-26 22:39:05 +0900

    feat: back to "/explore" button

--- front/src/components/Today/Today.js
+++ front/src/components/Today/Today.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import CelebrationBtn from "./CelebrationBtn";
-import TodayPhrase from "./TodayPhrase";
 import "../../css/today.css";
 import * as Api from "../../api";
+import { useNavigate } from "react-router-dom";
 
 function Today({ today }) {
   const month = today.getMonth() >= 9 ? String(today.getMonth() + 1) : '0' + String(today.getMonth() + 1);
@@ -12,8 +12,9 @@ function Today({ today }) {
   const dateQuery = month + "-" + day;
   const date = { month, day };
 
-  const [todayCharacter, setTodayCharacter] = useState([]);
+  const navigator = useNavigate();
 
+  const [todayCharacter, setTodayCharacter] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   async function getTodayCharacter() {
@@ -25,6 +26,9 @@ function Today({ today }) {
       console.error(error);
     }
   }
+  const handleBackClick = () => {
+    navigator("/explore");
+  };
 
   useEffect(() => {
     getTodayCharacter();
@@ -36,9 +40,9 @@ function Today({ today }) {
   }
   return (
     <div className="today">
+      <button className="back" onClick={handleBackClick}>{"< 뒤로가기"}</button>
       <div className="today-content">
-        <TodayPhrase date={date} villagers={villagers} />
-        {villagers.length > 0 ? <CelebrationBtn todayCharacter={todayCharacter} villagers={villagers} /> : <div className="phrase">내일 다시 와 줄래요?</div>}
+        {villagers.length > 0 ? <CelebrationBtn date={date} todayCharacter={todayCharacter} villagers={villagers} /> : <div className="phrase">내일 다시 와 줄래요?</div>}
       </div>
     </div>
   );
