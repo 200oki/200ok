2924d8f350a36048a42f264b279579d889eaa735
YunSeongJun <movomo@gmail.com>
2022-04-27 13:42:41 +0900

    docs: change field preview to block comment

--- back/src/db/schemas/character.js
+++ back/src/db/schemas/character.js
@@ -7,45 +7,47 @@ let raw = fs.readFileSync(
   path.resolve(__dirname, "./src/db/schemas/characters.json")
 );
 
-// const charactersMock = {
-//   admiral: {
-//     id: "admiral",
-//     special: false,
-//     name_en: "Admiral",
-//     name_ko: "일섭",
-//     image_icon: "https://acnhcdn.com/latest/NpcIcon/brd06.png",
-//     image_photo: "https://acnhcdn.com/latest/NpcBromide/NpcNmlBrd06.png",
-//     image_house:
-//       "https://acnhcdn.com/drivesync/render/houses/brd06_39_Admiral.png",
-//     species: "Bird",
-//     gender: "Male",
-//     gender_asia: "Male",
-//     personality: "Cranky",
-//     personality_subtype: "A",
-//     hobby: "Nature",
-//     birthday: "01-27",
-//     birthday_month: 1,
-//     birthday_day: 27,
-//     catchphrase: "aye aye",
-//     favorite_song: "Steep Hill",
-//     favorite_saying: "Only quitters give up.",
-//     styles: ["Cool"],
-//     colors: ["Black", "Blue"],
-//   },
-//   cyrus: {
-//     id: "cyrus",
-//     special: true,
-//     name_en: "Cyrus",
-//     name_ko: "리포",
-//     image_icon: "https://acnhcdn.com/latest/NpcIcon/alp.png",
-//     image_photo: "https://acnhcdn.com/latest/NpcPoster/NpcSpAlp.png",
-//     gender: "Male",
-//     gender_asia: "Male",
-//     birthday: "01-26",
-//     birthday_month: 1,
-//     birthday_day: 26,
-//   },
-// };
+/*
+const charactersMock = {
+  admiral: {
+    id: "admiral",
+    special: false,
+    name_en: "Admiral",
+    name_ko: "일섭",
+    image_icon: "https://acnhcdn.com/latest/NpcIcon/brd06.png",
+    image_photo: "https://acnhcdn.com/latest/NpcBromide/NpcNmlBrd06.png",
+    image_house:
+      "https://acnhcdn.com/drivesync/render/houses/brd06_39_Admiral.png",
+    species: "Bird",
+    gender: "Male",
+    gender_asia: "Male",
+    personality: "Cranky",
+    personality_subtype: "A",
+    hobby: "Nature",
+    birthday: "01-27",
+    birthday_month: 1,
+    birthday_day: 27,
+    catchphrase: "aye aye",
+    favorite_song: "Steep Hill",
+    favorite_saying: "Only quitters give up.",
+    styles: ["Cool"],
+    colors: ["Black", "Blue"],
+  },
+  cyrus: {
+    id: "cyrus",
+    special: true,
+    name_en: "Cyrus",
+    name_ko: "리포",
+    image_icon: "https://acnhcdn.com/latest/NpcIcon/alp.png",
+    image_photo: "https://acnhcdn.com/latest/NpcPoster/NpcSpAlp.png",
+    gender: "Male",
+    gender_asia: "Male",
+    birthday: "01-26",
+    birthday_month: 1,
+    birthday_day: 26,
+  },
+};
+ */
 
 /** `id`별 캐릭터 데이터를 담고 있습니다. */
 // const characters = charactersMock;
