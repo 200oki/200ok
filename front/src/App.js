a12167324e0d37962fe9066c5e9b833a8b02a8b2
kminzy <kminzy0704@gmail.com>
2022-04-27 11:09:51 +0900

    modify: add test route to App feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/App.js
+++ front/src/App.js
@@ -7,8 +7,9 @@ import Main from "./components/Main";
 import InputNickname from "./components/match/InputNickname";
 import Explore from "./components/explore/Explore";
 // import Quiz from "./components/quiz/Quiz";
+import MatchTest from "./components/match/MatchTest";
 import MatchResult from "./components/match/MatchResult";
-import Today from './components/Today/Today';
+import Today from "./components/Today/Today";
 
 function App() {
   const today = new Date();
@@ -19,10 +20,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path='/today' element={<Today today = {today}/>} />
+          <Route path="/today" element={<Today today={today} />} />
           <Route path="/explore" element={<Explore />} />
           <Route path="/match" element={<InputNickname />} />
           {/*<Route path="/quiz" element={<Quiz />} />*/}
+          <Route path="/matchTest" element={<MatchTest />} />
           <Route path="/matchResult" element={<MatchResult />} />
         </Routes>
       </Router>
