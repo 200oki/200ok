d5fc30673f0fcb33a82c6817de1170e93b4d7437
kminzy <kminzy0704@gmail.com>
2022-04-27 10:55:17 +0900

    modify: 코드리뷰 반영 feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/components/match/MatchResult.js
+++ front/src/components/match/MatchResult.js
@@ -2,11 +2,14 @@ import React, { useContext, useRef, useEffect, useState } from "react";
 import * as Api from "../../api";
 import { useNavigate } from "react-router-dom";
 import styled from "../../css/match.module.css";
+import MatchResultRank from "./MatchResultRank";
 import MatchResultComment from "./MatchResultComment";
 
 import { NicknameContext } from "../../context/NicknameContext";
 import { MatchCommentContext } from "../../context/MatchCommentContext";
 import { style } from "@mui/system";
+
+import ReactPageScroller from "react-page-scroller";
 
 const DIVIDER_HEIGHT = 5;
 
@@ -15,6 +18,9 @@ function MatchResult() {
   const { nickname, setNickname } = useContext(NicknameContext);
   const { setComment } = useContext(MatchCommentContext);
   const outerDivRef = useRef();
+
+  const [sample, setSample] = useState([]);
+  const [isloaded, setLoaded] = useState(false);
 
   const fetchCommentData = async () => {
     try {
@@ -26,8 +32,22 @@ function MatchResult() {
     }
   };
 
+  const getChar = async () => {
+    if (!isloaded) {
+      try {
+        const { data } = await Api.get(`characters?birthday=04-02`);
+        setSample([...Object.values(data.payload)]);
+      } catch (err) {
+        console.error(err);
+      }
+      setLoaded(true);
+    }
+    return () => {};
+  };
+
   useEffect(() => {
     fetchCommentData();
+    getChar();
   }, []);
 
   const wheelHandler = (e) => {
@@ -35,38 +55,38 @@ function MatchResult() {
 
     const { deltaY } = e;
     const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
-    const pageHeight = window.innerHeight;
+    const PAGE_HEIGHT = window.innerHeight;
 
     if (deltaY > 0) {
       // 스크롤 내릴 때
-      if (scrollTop >= 0 && scrollTop < pageHeight) {
+      if (scrollTop >= 0 && scrollTop < PAGE_HEIGHT) {
         outerDivRef.current.scrollTo({
-          top: pageHeight + DIVIDER_HEIGHT,
+          top: PAGE_HEIGHT + DIVIDER_HEIGHT,
           left: 0,
           behavior: "smooth",
         });
-      } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+      } else if (scrollTop >= PAGE_HEIGHT && scrollTop < PAGE_HEIGHT * 2) {
         outerDivRef.current.scrollTo({
-          top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+          top: PAGE_HEIGHT * 2 + DIVIDER_HEIGHT * 2,
           left: 0,
           behavior: "smooth",
         });
       } else {
         outerDivRef.current.scrollTo({
-          top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+          top: PAGE_HEIGHT * 2 + DIVIDER_HEIGHT * 2,
           left: 0,
           behavior: "smooth",
         });
       }
     } else {
       // 스크롤 올릴 때
-      if (scrollTop >= 0 && scrollTop < pageHeight) {
+      if (scrollTop >= 0 && scrollTop < PAGE_HEIGHT) {
         outerDivRef.current.scrollTo({
           top: 0,
           left: 0,
           behavior: "smooth",
         });
-      } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+      } else if (scrollTop >= PAGE_HEIGHT && scrollTop < PAGE_HEIGHT * 2) {
         outerDivRef.current.scrollTo({
           top: 0,
           left: 0,
@@ -74,7 +94,7 @@ function MatchResult() {
         });
       } else {
         outerDivRef.current.scrollTo({
-          top: pageHeight + DIVIDER_HEIGHT,
+          top: PAGE_HEIGHT + DIVIDER_HEIGHT,
           left: 0,
           behavior: "smooth",
         });
@@ -85,32 +105,34 @@ function MatchResult() {
   const goToPosition = (e) => {
     e.preventDefault();
 
-    const pageHeight = window.innerHeight;
+    const PAGE_HEIGHT = window.innerHeight;
 
     if (e.target.innerText === "랭킹") {
       outerDivRef.current.scrollTo({
-        top: pageHeight + DIVIDER_HEIGHT,
+        top: PAGE_HEIGHT + DIVIDER_HEIGHT,
         left: 0,
         behavior: "smooth",
       });
     } else {
       outerDivRef.current.scrollTo({
-        top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+        top: PAGE_HEIGHT * 2 + DIVIDER_HEIGHT * 2,
         left: 0,
         behavior: "smooth",
       });
     }
   };
 
-  useEffect(() => {
-    const outerDivRefCurrent = outerDivRef.current;
+  // useEffect(() => {
+  //   const outerDivRefCurrent = outerDivRef.current;
 
-    outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+  //   if (outerDivRef && outerDivRefCurrent) {
+  //     outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+  //   }
 
-    return () => {
-      outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
-    };
-  }, []);
+  //   return () => {
+  //     outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
+  //   };
+  // }, []);
 
   const goToFirstPage = () => {
     setNickname("");
@@ -119,6 +141,7 @@ function MatchResult() {
 
   return (
     <div className={styled.outer} ref={outerDivRef}>
+      {/* <ReactPageScroller blockScrollUp> */}
       <div className={styled.inner}>
         <div className={styled.imgWrapper}>
           <img src="/images/Aurora.png" />
@@ -133,16 +156,17 @@ function MatchResult() {
         <div className={styled.btnsWrapper}>
           <button>공유하기</button>
           <button onClick={goToFirstPage}>다시하기</button>
-          <button onClick={(e) => goToPosition(e)}>랭킹</button>
-          <button onClick={(e) => goToPosition(e)}>반응 남기기</button>
+          <button onClick={goToPosition}>랭킹</button>
+          <button onClick={goToPosition}>반응 남기기</button>
         </div>
       </div>
-      <div className={styled.divider}></div>
-      <div className={styled.inner}>랭킹 영역</div>
-      <div className={styled.divider}></div>
+      <div className={styled.inner}>
+        <MatchResultRank sample={sample} />
+      </div>
       <div className={styled.inner}>
         <MatchResultComment />
       </div>
+      {/* </ReactPageScroller> */}
     </div>
   );
 }
