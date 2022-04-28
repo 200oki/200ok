1b947b1d9042f242916ac7c6ed3b51c79fe77d42
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-04-29 01:40:13 +0900

    feat: add css feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/components/Statistics/StatBtn.js
+++ front/src/components/Statistics/StatBtn.js
@@ -3,12 +3,14 @@ import { useStyles } from "../../utils/useStyles";
 import StyledItem from "../StyledItem";
 import { statMenuList } from "../../utils/util";
 import { useNavigate } from "react-router-dom";
+import "../../css/stats.css";
 
 const StatBtn = () => {
   const classes = useStyles();
   const navigator = useNavigate();
 
   const handleClick = (e) => {
+    // eslint-disable-next-line default-case
     switch (e.target.innerText) {
       case "성별 분포":
         return navigator("/stats/gender");
@@ -27,12 +29,14 @@ const StatBtn = () => {
         break;
     }
   };
+  
   return (
-    <div>
-      <div className={"stats"}>
-        {statMenuList.map((item, idx) => {
-          return idx >= 2 ? (
-            <div className="firstDiv">
+    <>
+      <div className="firstDiv">
+        {statMenuList
+          .filter((menu) => menu.indexOf(menu) <= 2)
+          .map((item, idx) => {
+            return (
               <StyledItem
                 key={idx}
                 type="whiteItem"
@@ -40,22 +44,27 @@ const StatBtn = () => {
                 className={classes.menuItem}
                 onClick={handleClick}
               />
-            </div>
-          ) : (
-            <div className="secondDiv">
-              <StyledItem
-                key={idx}
-                type="whiteItem"
-                content={item}
-                className={classes.menuItem}
-                onClick={handleClick}
-              />
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
-    </div>
+      <div className="secondDiv">
+        {statMenuList
+            .filter((menu) => menu.indexOf(menu) > 2)
+            .map((item, idx) => {
+              return (
+                  <StyledItem
+                      key={idx}
+                      type="whiteItem"
+                      content={item}
+                      className={classes.menuItem}
+                      onClick={handleClick}
+                  />
+              );
+            })}
+      </div>
+    </>
   );
 };
+
 
 export default StatBtn;
