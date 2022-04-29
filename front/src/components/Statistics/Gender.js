f42010a1c294dda34c7dadbdf3b161c62cae85ae
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-04-29 11:37:23 +0900

    modify: change variable name feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/components/Statistics/Gender.js
+++ front/src/components/Statistics/Gender.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
+import * as Api from "../../api";
 import styled from "../../css/match.module.css";
 import StatBtn from "./StatBtn.js";
-import * as Api from "../../api";
 import {Chart, registerables} from "chart.js";
 import { Pie } from 'react-chartjs-2';
 Chart.register(...registerables)
@@ -9,13 +9,13 @@ Chart.register(...registerables)
 // 젠더는 npc도 포함이라 더 갯수가 많다고 적어주기!
 
 const Gender = () => {
-  const [genderList, setGenderList] = useState([]);
+  const [dataList, setDataList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   
-  async function getGenderList() {
+  async function getDataList() {
     try {
       const { data } = await Api.get('stats', '?groupName=gender');
-      setGenderList([...Object.values(data.payload)]);
+      setDataList([...Object.values(data.payload)]);
       setIsLoading(false);
     } catch (error) {
       console.error(error);
@@ -23,47 +23,47 @@ const Gender = () => {
   }
 
   useEffect(() => {
-    getGenderList();
+    getDataList();
   }, []);
 
   return (
-    <div className={styled.Wrapper}>
-      <StatBtn />
-      <Pie 
-        data={{
-          labels: genderList[1],
-            datasets: [
-            {
-              label: '# Gender',
-              data: genderList[2],
-              backgroundColor: [
-                'rgba(242, 214, 74, 0.5)',
-                'rgba(54, 162, 235, 0.3)'
-              ],
-              borderColor: [
-                'rgba(205, 173, 14, 1)',
-                'rgba(54, 162, 235, 1)'
-              ],
-              borderWidth: 1
-              }
-            ]
-        }}
-        height={400}
-        width={400}
-        options= {{
-            responsive: false,
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    beginAtZero: true,
-                  },
+    <Pie 
+      data={{
+        labels: dataList[1],
+          datasets: [
+          {
+            label: dataList[1],
+            data: dataList[2],
+            backgroundColor: [
+              'rgba(242, 214, 74, 0.5)',
+              'rgba(54, 162, 235, 0.3)'
+            ],
+            borderColor: [
+              'rgba(205, 173, 14, 1)',
+              'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1,
+            }
+          ]
+      }}
+      height={400}
+      width={400}
+      options= {{
+          responsive: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
                 },
-              ],
-            },
-        }}
-      />
-    </div>
+              },
+            ],
+          },
+          chartArea: {
+            backgroundColor: 'rgba(0, 0, 0, 0.7)'
+          },
+      }}
+    />
   );
 }
 
