628713c65d415ee5fdcec8dccbe636db7062f99d
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-04-29 21:56:50 +0900

    modify: add route feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/App.js
+++ front/src/App.js
@@ -8,7 +8,12 @@ import Explore from "./components/explore/Explore";
 import MatchResult from "./components/match/MatchResult";
 import Today from "./components/Today/Today";
 import Bestiary from "./components/Bestiary/Bestiary";
-import Gender from "./components/Statistics/Gender";
+import Stat from "./components/Statistics/Stat";
+import GenderChart from "./components/Statistics/GenderChart";
+import SpeciesChart from "./components/Statistics/SpeciesChart";
+import PersonalityChart from "./components/Statistics/PersonalityChart";
+import HobbyChart from "./components/Statistics/HobbyChart";
+import StyleChart from "./components/Statistics/StyleChart";
 
 function App() {
   const today = new Date();
@@ -23,7 +28,13 @@ function App() {
           <Route path="/match" element={<InputNickname />} />
           <Route path="/game" element={<InputNickname />} />
           <Route path="/matchResult" element={<MatchResult />} />
-          <Route path="/stats/gender" element={<Gender />} />
+          <Route path="/stats" element={<Stat />}>
+            <Route path="gender" element={<GenderChart />} />
+            <Route path="species" element={<SpeciesChart />} />
+            <Route path="personality" element={<PersonalityChart />} />
+            <Route path="hobby" element={<HobbyChart />} />
+            <Route path="style" element={<StyleChart />} />
+          </Route>
         </Routes>
       </Router>
     </div>
