8ee1e885c89dc9ab9b0b14ed4e16973db6d99428
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-04-29 17:31:55 +0900

    feat: add species chart feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

---
+++ front/src/components/Statistics/Species.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import * as Api from "../../api";
+import {Chart, registerables} from "chart.js";
+import { Bar } from 'react-chartjs-2';
+Chart.register(...registerables)
+
+const Species = () => {
+  const [dataList, setDataList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  async function getDataList() {
+    try {
+      const { data } = await Api.get('stats', '?groupName=species');
+      setDataList([...Object.values(data.payload)]);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getDataList();
+  }, []);
+
+  return (
+    <Bar 
+      data={{
+        labels: dataList[1],
+          datasets: [
+          {
+            label: dataList[1],
+            data: dataList[2],
+            backgroundColor: [
+              'rgba(242, 214, 74, 0.5)',
+              'rgba(54, 162, 235, 0.3)'
+            ],
+            borderColor: [
+              'rgba(205, 173, 14, 1)',
+              'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+            }
+          ]
+      }}
+      height={400}
+      width={800}
+      options= {{
+          responsive: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+      }}
+    />
+  );
+}
+
+export default Species;
