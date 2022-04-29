30b8ce29122b85fd399876072bb4e417010cde01
kminzy <kminzy0704@gmail.com>
2022-04-29 23:09:41 +0900

    feat: add select hobby & style component feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

---
+++ front/src/components/match/SelectHobbyStyle.js
@@ -0,0 +1,74 @@
+import React, { useState, useContext } from "react";
+import styled from "../../css/match.module.css";
+
+import { NicknameContext } from "../../context/NicknameContext";
+
+const SelectHobbyStyle = ({ array, nextSlide }) => {
+  const { nickname } = useContext(NicknameContext);
+
+  const [hobby, setHobby] = useState(null);
+  const [style, setStyle] = useState(null);
+
+  const handleHobbyStyleChange = (e) => {
+    if (array[0] === "취미") {
+      setHobby(e.target.value);
+    } else {
+      setStyle(e.target.value);
+    }
+  };
+
+  return (
+    <div className={`${styled.testContent} ${styled.Wrapper}`}>
+      {array[0] === "취미" ? (
+        <div className={styled.testTitle}>
+          {nickname}님의 취미는 무엇인가요?
+        </div>
+      ) : (
+        <div className={styled.testTitle}>
+          {nickname}님의 스타일을 알려주세요!
+        </div>
+      )}
+      <form className={styled.hobbyStyleForm}>
+        <div>
+          <div className={styled.hobbyStyleDiv}>
+            {array
+              .filter((a) => array.indexOf(a) !== 0)
+              .map((p, i) => (
+                <div key={`input${i}`} className={styled.hobbyStyleRadioBtn}>
+                  <input
+                    type="radio"
+                    value={i}
+                    id={array[0] === "취미" ? `hobby${i}` : `style${i}`}
+                    name={array[0]}
+                    onChange={handleHobbyStyleChange}
+                  />
+                  <label
+                    htmlFor={array[0] === "취미" ? `hobby${i}` : `style${i}`}
+                  >
+                    {p}
+                  </label>
+                </div>
+              ))}
+          </div>
+        </div>
+      </form>
+      {array[0] === "취미" ? (
+        <button
+          className={hobby !== null ? styled.btnActive : styled.btnHidden}
+          onClick={nextSlide}
+        >
+          다음
+        </button>
+      ) : (
+        <button
+          className={style !== null ? styled.btnActive : styled.btnHidden}
+          onClick={nextSlide}
+        >
+          다음
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default SelectHobbyStyle;
