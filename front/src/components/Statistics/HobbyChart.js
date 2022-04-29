f8b03588852867eb3e59240cd484f528051b0bee
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-04-30 04:03:30 +0900

    modify: add style feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/components/Statistics/HobbyChart.js
+++ front/src/components/Statistics/HobbyChart.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import * as Api from "../../api";
 import { Chart, registerables } from "chart.js";
-import { Pie } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 Chart.register(...registerables)
 
 const HobbyChart = () => {
@@ -13,6 +13,7 @@ const HobbyChart = () => {
       const { data } = await Api.get('stats', '?groupName=hobby');
       setDataList([...Object.values(data.payload)]);
       setIsLoading(false);
+      console.log(dataList[1]);
     } catch (error) {
       console.error(error);
     }
@@ -23,7 +24,7 @@ const HobbyChart = () => {
   }, []);
 
   return (
-    <Pie className="graphBack"
+    <Bar className="graphBack"
       data={{
         labels: dataList[1],
           datasets: [
@@ -31,19 +32,27 @@ const HobbyChart = () => {
             label: dataList[1],
             data: dataList[2],
             backgroundColor: [
-              'rgba(242, 214, 74, 0.5)',
-              'rgba(54, 162, 235, 0.3)'
+              'rgba(242, 34, 110, 0.2)',
+              'rgba(5, 151, 242, 0.3)',
+              'rgba(3, 166, 60, 0.3)',
+              'rgba(242, 226, 5, 0.3)',
+              'rgba(242, 135, 5, 0.3)',
+              'rgba(134, 5, 240, 0.15)',
             ],
             borderColor: [
-              'rgba(205, 173, 14, 1)',
-              'rgba(54, 162, 235, 1)'
+              'rgba(242, 34, 110, 1)',
+              'rgba(5, 151, 242, 1)',
+              'rgba(3, 166, 60, 1)',
+              'rgba(242, 226, 5, 1)',
+              'rgba(242, 135, 5, 1)',
+              'rgba(134, 5, 240, 1)',
             ],
             borderWidth: 1,
             }
           ]
       }}
       height={500}
-      width={500}
+      width={1200}
       options= {{
           responsive: false,
           scales: {
