ea15c49728bc91b2b53c87b96aa8843571504fa7
hongloans <loans93@gmail.com>
2022-04-30 18:40:34 +0900

    feat: import things

--- front/src/components/Bestiary/VillagerDetail.jsx
+++ front/src/components/Bestiary/VillagerDetail.jsx
@@ -1,13 +1,28 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import * as Api from "../../api";
+import styled from "styled-components";
+import BackButton from "../common/BackButton";
+import HomeButton from "../common/HomeButton";
 
 const VillagerDetail = () => {
+  const [villager, setVillager] = useState({});
   const { id } = useParams();
-  // const getVillager = async () => {
-  //   const { data } = await Api
-  // }
-
-  return <div>여기에 디테일</div>;
+  const getVillager = async () => {
+    const { data } = await Api.get(`characters/${id}`);
+    setVillager(data.payload);
+  };
+  useEffect(() => {
+    getVillager();
+  }, []);
+  return (
+    <div>
+      <nav>
+        <button>뒤로</button>
+        <button>홈</button>
+      </nav>
+      <div></div>
+    </div>
+  );
 };
 export default VillagerDetail;
