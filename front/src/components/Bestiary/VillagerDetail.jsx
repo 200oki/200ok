b5a280cf4ee8426867cf89de8061a14289aea0fb
hongloans <loans93@gmail.com>
2022-05-01 02:36:23 +0900

    style: make styled components in detail page

--- front/src/components/Bestiary/VillagerDetail.jsx
+++ front/src/components/Bestiary/VillagerDetail.jsx
@@ -20,6 +20,39 @@ const Wrapper = styled.div`
   right: 50px;
 `;
 
+const Container = styled.div`
+  position: relative;
+&::before {
+  background-image: url("/images/leafBgImg.jpg");
+  content: " ";
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  opacity: 0.5;
+  background-position: 50% 0;
+  z-index: -100;
+`;
+const pop = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+const Content = styled.div`
+  position: relative;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  animation: ${pop} 1s linear forwards;
+`;
+
 const VillagerDetail = () => {
   const [villager, setVillager] = useState({});
   const { id } = useParams();
