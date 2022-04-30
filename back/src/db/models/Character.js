7a5d88b574cb67121cadeec3d2875987e44304df
YunSeongJun <movomo@gmail.com>
2022-04-30 17:58:59 +0900

    docs: update allowed search field list

--- back/src/db/models/Character.js
+++ back/src/db/models/Character.js
@@ -39,6 +39,7 @@ class Character {
    *  - `tier`
    *  - `hobby`
    *  - `personality`
+   *  - `species`
    *  - `colors`
    *  - `styles`
    *
@@ -92,6 +93,7 @@ class Character {
    *  - `tier`
    *  - `hobby`
    *  - `personality`
+   *  - `species`
    *  - `colors`
    *  - `styles`
    *
