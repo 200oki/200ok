1cd001958c2173f970cd85af0199c12d258d1c20
hongloans <loans93@gmail.com>
2022-04-30 18:01:32 +0900

    feat: import api

--- front/src/components/Bestiary/VillagerDetail.jsx
+++ front/src/components/Bestiary/VillagerDetail.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import * as Api from "../../api";
 
 const VillagerDetail = () => {
   const { id } = useParams();
+  // const getVillager = async () => {
+  //   const { data } = await Api
+  // }
 
   return <div>여기에 디테일</div>;
 };
