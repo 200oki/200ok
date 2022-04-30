be60edeccfef1f165c5860167a8fd6a7cb0fcc4d
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-04-30 21:07:38 +0900

    modify: stacked chart feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/components/Statistics/HobbyChart.js
+++ front/src/components/Statistics/HobbyChart.js
@@ -1,71 +1,100 @@
-import React, { useState, useEffect } from "react";
-import * as Api from "../../api";
-import { Chart, registerables } from "chart.js";
-import { Bar } from 'react-chartjs-2';
-Chart.register(...registerables)
+import React from "react";
+import { Bar, char } from "react-chartjs-2";
 
 const HobbyChart = () => {
-  const [dataList, setDataList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  
-  async function getDataList() {
-    try {
-      const { data } = await Api.get('stats', '?groupName=hobby');
-      setDataList([...Object.values(data.payload)]);
-      setIsLoading(false);
-      console.log(dataList[1]); // 삭제 !!!
-
-    } catch (error) {
-      console.error(error);
+  const data = {
+    labels: ["Organic", "Sponsored", "Organic", "Sponsored"],
+    previousDate: {
+      label: "08/10/2019 - 09/30/2019",
+      dataSet: [10000, 150000, 10000, 150000]
+    },
+    currentDate: {
+      label: "10/01/2019 - 11/20/2019",
+      dataSet: [10000, 225000, 10000, 225000]
     }
-  }
-
-  useEffect(() => {
-    getDataList();
-  }, []);
+  };
 
   return (
-    <Bar className="graphBack"
-      data={{
-        labels: dataList[1],
+    <div className="App">
+      <Bar
+        pointStyle="star"
+        data={{
+          labels: data.labels,
+          responsive: true,
+          offset: true,
           datasets: [
-          {
-            label: '# Hobby',
-            data: dataList[2],
-            backgroundColor: [
-              'rgba(242, 34, 110, 0.2)',
-              'rgba(5, 151, 242, 0.3)',
-              'rgba(3, 166, 60, 0.3)',
-              'rgba(242, 226, 5, 0.3)',
-              'rgba(242, 135, 5, 0.3)',
-              'rgba(134, 5, 240, 0.15)',
-            ],
-            borderColor: [
-              'rgba(242, 34, 110, 1)',
-              'rgba(5, 151, 242, 1)',
-              'rgba(3, 166, 60, 1)',
-              'rgba(242, 226, 5, 1)',
-              'rgba(242, 135, 5, 1)',
-              'rgba(134, 5, 240, 1)',
-            ],
-            borderWidth: 1,
+            {
+              label: "Mobile",
+              pointStyle: "rectRounded",
+              backgroundColor: "#6ED3FF",
+              barThickness: 40,
+              categoryPercentage: 1,
+              data: data.previousDate.dataSet //From API
             },
             {
-              label: "# 여성",
-              data: [30, 40, 35, 22, 50, 43],
-            },
-            {
-              label: "# 남성",
-              data: [20, 30, 45, 52, 20, 13],
-            },
-          ],
-      }}
-      height={500}
-      width={1200}
-      options= {{
-          responsive: false,
-      }}
-    />
+              label: "Desktop",
+              backgroundColor: "#1497FF",
+              barThickness: 40,
+              categoryPercentage: 1,
+              pointStyle: "triangle",
+              data: data.currentDate.dataSet //From API
+            }
+          ]
+        }}
+        height={220}
+        options={{
+          offsetGridLines: true,
+          drawTicks: true,
+          layout: {
+            padding: {
+              top: 30,
+              right: 40,
+              bottom: 40
+            }
+          },
+          legend: {
+            display: true,
+            position: "right",
+            align: "start",
+            labels: {
+              usePointStyle: true
+            }
+          },
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            xAxes: [
+              {
+                stacked: true,
+                ticks: {
+                  padding: 5
+                },
+                gridLines: {
+                  display: false
+                }
+              }
+            ],
+            yAxes: [
+              {
+                stacked: false,
+                gridLines: {
+                  drawBorder: false
+                },
+                ticks: {
+                  beginAtZero: true,
+                  maxTicksLimit: 6,
+                  padding: 20,
+                  callback(n) {
+                    if (n < 1e3) return n;
+                    if (n >= 1e3) return +(n / 1e3).toFixed(1) + "K";
+                  }
+                }
+              }
+            ]
+          }
+        }}
+      />
+    </div>
   );
 }
 
