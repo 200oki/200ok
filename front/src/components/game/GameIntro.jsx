08a5ade5b1818af4abf7a5136a7532455b4761f3
Changm-L <chang6585@naver.com>
2022-04-30 19:07:27 +0900

    fix : 화면 비율 수정

--- front/src/components/game/GameIntro.jsx
+++ front/src/components/game/GameIntro.jsx
@@ -1,51 +1,37 @@
-import "../../css/GameIntro.css";
 import { useStyles } from "../../utils/useStyles";
-import { Typography } from "@mui/material";
+import "../../css/GameIntro.css";
 import { useNavigate } from "react-router-dom";
 
-const GameIntro = () => {
+const GameResult = () => {
   const classes = useStyles();
   const navigator = useNavigate();
-  const typoStyles = {
-    fontFamily: "TmoneyRoundWindExtraBold",
-    fontSize: "1.5rem",
-  };
+
   const handleClick = () => {
     navigator("/game-start");
   };
   return (
     <div className="gameResultRoot">
       <img
-        src="images/gameIntro.png"
-        className={classes.gameIntroImg}
-        alt="DJ K.K"
+        src="images/game_intro.png"
+        alt="raccoon"
+        className={"gameIntroImg"}
       />
       <div className="contentRoot">
-        <img
-          src="images/gameIntroContent.png"
-          alt="comment"
-          className="gameIntroCommentImg"
-        />
-        <img
-          className={`${classes.bottomArrow} blinkImg`}
-          src="images/triangleBottomArrow.png"
-          alt="arrow"
-        />
-        <Typography
-          sx={typoStyles}
-          variant={"body1"}
-          className="gameIntroComment"
-        >
-          ♪ ~ ♪ ♪ ♪ ~ ♪ ♪
-        </Typography>
+        <div className={"gameIntroComment"}>
+          <img src="images/game_intro_content.png" alt="comment" />
+          <img
+            className={`${classes.introBottomArrow} blinkImg`}
+            src="images/triangleBottomArrow.png"
+            alt="arrow"
+          />
+        </div>
+
         <div className="btnWrapper">
-          <button className="introBtn" onClick={handleClick}>
-            게임 시작
-          </button>
+          <button onClick={handleClick}>게임 시작</button>
         </div>
       </div>
     </div>
   );
 };
 
-export default GameIntro;
+export default GameResult;
