f89ed5dcd0b7e2393ca2fa9cc9b7ed274d5c621a
Changm-L <chang6585@naver.com>
2022-05-02 02:33:12 +0900

    feat : add card choice logic

--- front/src/components/game/FlashcardList.jsx
+++ front/src/components/game/FlashcardList.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FlashCard from "./Flashcard";
 import "../../css/flipcard.css";
 const FlashcardList = ({ flashcard }) => {
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const handleChoice = (elem) => {
+    choiceOne ? setChoiceTwo(elem) : setChoiceOne(elem);
+  };
+
+  useEffect(() => {
+    console.log("one", choiceOne, "two", choiceTwo);
+  }, [choiceOne, choiceTwo]);
+
   return (
     <div className="card-grid">
       {flashcard.map((flashcard, idx) => {
-        return <FlashCard flashcard={flashcard} key={idx} />;
+        return (
+          <FlashCard
+            handleChoice={handleChoice}
+            flashcard={flashcard}
+            key={idx}
+          />
+        );
       })}
     </div>
   );
