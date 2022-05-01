3d93e9231e3ac11f814d6b3801ca29b6f4651d73
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-02 06:16:42 +0900

    docs: add guestbook post swagger

--- back/src/routers/guestbookRouter.js
+++ back/src/routers/guestbookRouter.js
@@ -6,6 +6,84 @@ import * as status from "../utils/status.js";
 
 const guestbookRouter = Router();
 
+/**
+ *  @swagger
+ *  tags:
+ *    name: Guestbook
+ *    description: Guestbook MVP
+ */
+/**
+ * @swagger
+ * /guestbook:
+ *   post:
+ *    summary: 방명록 API
+ *    description: 방명록을 생성할 때 사용하는 API 입니다.
+ *    tags: [Guestbook]
+ *    requestBody:
+ *      x-name: body
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            required:
+ *            - content
+ *            properties:
+ *              content:
+ *                type: string
+ *                example: tester
+ *    responses:
+ *      201:
+ *        description: 방명록 생성
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                success:
+ *                  type: string
+ *                  example: true
+ *                payload:
+ *                  type: object
+ *                  properties:
+ *                    id:
+ *                      type: number
+ *                      description: 1, 2, 3, 4, 5...
+ *                      example: 1
+ *                    content:
+ *                      type: string
+ *                      description: 방명록 내용
+ *                      example: 안녕
+ *      400:
+ *        description: 방명록 생성 오류
+ *        content:
+ *         application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                success:
+ *                  type: string
+ *                  example: false
+ *                error:
+ *                  type: object
+ *                  properties:
+ *                    code:
+ *                      type: integer
+ *                      description: http status
+ *                      example: 400
+ *                    message:
+ *                      type: string
+ *                      description: 오류 내용
+ *                      example: 글을 입력해주세요.
+ *                    detail:
+ *                      type: object
+ *                      description: 오류 세부 사항
+ *                      properties:
+ *                        msg:
+ *                          type: string
+ *                          description: 오류 내용
+ *                          example: 글을 입력해주세요.
+ *                          
+*/
 guestbookRouter.post(
   "/guestbooks",
   [
