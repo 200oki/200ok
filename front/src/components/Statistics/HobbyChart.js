aa09d6df22e87371a5e62a1d309f4a86fe25d742
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-02 01:52:11 +0900

    modify: add style

--- front/src/components/Statistics/HobbyChart.js
+++ front/src/components/Statistics/HobbyChart.js
@@ -1,141 +1,141 @@
-// 아직 작성중입니다!!
-// import React, { useState, useEffect } from "react";
-// import * as Api from "../../api";
-// import { ResponsiveBar } from '@nivo/bar'
+import React from 'react';
+import { ResponsiveBar } from '@nivo/bar'
 
-// const HobbyChart = () => {
-//   const [dataList, setDataList] = useState([]);
-//   const [isLoading, setIsLoading] = useState(true);
-  
-//   async function getDataList() {
-//     try {
-//       const { data } = await Api.get('stats', '?groupName=gender');
-//       setDataList([...Object.values(data.payload)]);
-//       setIsLoading(false);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
+// make sure parent container have a defined height when using
+// responsive component, otherwise height will be 0 and
+// no chart will be rendered.
+// website examples showcase many properties,
+// you'll often use just a few of them.
+const HobbyChart = () => {
+  const data = [
+    {
+      "hobby": "교육",
+      "female": 32,
+      "femaleColor": "hsl(205, 63%, 84.1%)",
+      "male": 32,
+      "maleColor": "hsl(52, 70%, 50%)",
+    },
+    {
+      "hobby": "놀이",
+      "female": 16,
+      "femaleColor": "hsl(205, 63%, 84.1%)",
+      "male": 49,
+      "maleColor": "hsl(245, 70%, 50%)",
+    },
+    {
+      "hobby": "운동",
+      "female": 12,
+      "femaleColor": "hsl(205, 63%, 84.1%)",
+      "male": 54,
+      "maleColor": "hsl(187, 70%, 50%)",
+    },
+    {
+      "hobby": "음악",
+      "female": 34,
+      "femaleColor": "hsl(205, 63%, 84.1%)",
+      "male": 27,
+      "maleColor": "hsl(325, 70%, 50%)",
+    },
+    {
+      "hobby": "자연",
+      "female": 27,
+      "femaleColor": "hsl(205, 63%, 84.1%)",
+      "male": 39,
+      "maleColor": "hsl(17, 70%, 50%)",
+    },
+    {
+      "hobby": "패션",
+      "female": 66,
+      "femaleColor": "hsl(205, 63%, 84.1%)",
+      "male": 0,
+      "maleColor": "hsl(154, 70%, 50%)",
+    },
+  ];
+  return (
+    <div style={{ width: '1200px', height: '500px' }}>
+    <ResponsiveBar
+      data={data}
+      keys={[
+        'female',
+        'male',
+      ]}
+      indexBy="hobby"
+      // margin={{ top: 0, right: 300, bottom: 0, left: 300 }}
+      margin={{ top: 10, right: 300, bottom: 40, left: 300 }}
+      padding={0.3}
+      valueScale={{ type: 'linear' }}
+      indexScale={{ type: 'band', round: true }}
+      colors={{ scheme: 'nivo' }}
+      borderColor={{
+        from: 'color',
+        modifiers: [
+          [
+            'darker',
+            1.6
+          ]
+        ]
+      }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Hobby',
+        legendPosition: 'middle',
+        legendOffset: 32
+      }}
+      axisLeft={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'food',
+        legendPosition: 'middle',
+        legendOffset: -40
+      }}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      labelTextColor={{
+        from: 'color',
+        modifiers: [
+          [
+            'darker',
+            1.6
+          ]
+        ]
+      }}
+      legends={[
+        {
+          dataFrom: 'keys',
+          anchor: 'bottom-right',
+          direction: 'column',
+          justify: false,
+          translateX: 120,
+          translateY: 0,
+          itemsSpacing: 2,
+          itemWidth: 100,
+          itemHeight: 20,
+          itemDirection: 'left-to-right',
+          itemOpacity: 0.85,
+          symbolSize: 20,
+          effects: [
+            {
+              on: 'hover',
+              style: {
+                itemOpacity: 1
+                }
+              }
+            ]
+          }
+        ]
+      }
+      role="application"
+      ariaLabel="Nivo bar chart demo"
+      barAriaLabel={function(e){return e.id+": "+e.formattedValue+" Hobby: "+e.indexValue}}
+    />
+    </div>
+  );
+}
 
-//   useEffect(() => {
-//     getDataList();
-//   }, []);
 
-//   return (
-//     <ResponsiveBar className="graphBack"
-//         data={dataList}
-//         keys={[
-//           'Female', 
-//           'male'
-//         ]}
-//         indexBy="country"
-//         margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-//         padding={0.3}
-//         valueScale={{ type: 'linear' }}
-//         indexScale={{ type: 'band', round: true }}
-//         colors={{ scheme: 'nivo' }}
-//         defs={[
-//             {
-//                 id: 'dots',
-//                 type: 'patternDots',
-//                 background: 'inherit',
-//                 color: '#38bcb2',
-//                 size: 4,
-//                 padding: 1,
-//                 stagger: true
-//             },
-//             {
-//                 id: 'lines',
-//                 type: 'patternLines',
-//                 background: 'inherit',
-//                 color: '#eed312',
-//                 rotation: -45,
-//                 lineWidth: 6,
-//                 spacing: 10
-//             }
-//         ]}
-//         fill={[
-//             {
-//                 match: {
-//                     id: 'fries'
-//                 },
-//                 id: 'lines'
-//             },
-//             {
-//                 match: {
-//                     id: 'sandwich'
-//                 },
-//                 id: 'lines'
-//             }
-//         ]}
-//         borderColor={{
-//             from: 'color',
-//             modifiers: [
-//                 [
-//                     'darker',
-//                     1.6
-//                 ]
-//             ]
-//         }}
-//         axisTop={null}
-//         axisRight={null}
-//         axisBottom={{
-//             tickSize: 5,
-//             tickPadding: 5,
-//             tickRotation: 0,
-//             legend: 'country',
-//             legendPosition: 'middle',
-//             legendOffset: 32
-//         }}
-//         axisLeft={{
-//             tickSize: 5,
-//             tickPadding: 5,
-//             tickRotation: 0,
-//             legend: 'food',
-//             legendPosition: 'middle',
-//             legendOffset: -40
-//         }}
-//         labelSkipWidth={12}
-//         labelSkipHeight={12}
-//         labelTextColor={{
-//             from: 'color',
-//             modifiers: [
-//                 [
-//                     'darker',
-//                     1.6
-//                 ]
-//             ]
-//         }}
-//         legends={[
-//             {
-//                 dataFrom: 'keys',
-//                 anchor: 'bottom-right',
-//                 direction: 'column',
-//                 justify: false,
-//                 translateX: 120,
-//                 translateY: 0,
-//                 itemsSpacing: 2,
-//                 itemWidth: 100,
-//                 itemHeight: 20,
-//                 itemDirection: 'left-to-right',
-//                 itemOpacity: 0.85,
-//                 symbolSize: 20,
-//                 effects: [
-//                     {
-//                         on: 'hover',
-//                         style: {
-//                             itemOpacity: 1
-//                         }
-//                     }
-//                 ]
-//             }
-//         ]}
-//         role="application"
-//         ariaLabel="Nivo bar chart demo"
-//         barAriaLabel={function(e){return e.id+": "+e.formattedValue+" in country: "+e.indexValue}}
-//     />
-//   );
-// }
-
-
-// export default HobbyChart;
+export default HobbyChart;
