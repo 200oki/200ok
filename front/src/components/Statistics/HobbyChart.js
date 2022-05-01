54d1c8a41a4b8c819b6a2e7ff08c85c99dc7563f
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-01 20:20:11 +0900

    modify: change hobby chart

--- front/src/components/Statistics/HobbyChart.js
+++ front/src/components/Statistics/HobbyChart.js
@@ -1,101 +1,140 @@
-import React from "react";
-import { Bar, char } from "react-chartjs-2";
+// import React, { useState, useEffect } from "react";
+// import * as Api from "../../api";
+// import { ResponsiveBar } from '@nivo/bar'
 
-const HobbyChart = () => {
-  const data = {
-    labels: ["Organic", "Sponsored", "Organic", "Sponsored"],
-    previousDate: {
-      label: "08/10/2019 - 09/30/2019",
-      dataSet: [10000, 150000, 10000, 150000]
-    },
-    currentDate: {
-      label: "10/01/2019 - 11/20/2019",
-      dataSet: [10000, 225000, 10000, 225000]
-    }
-  };
+// const HobbyChart = () => {
+//   const [dataList, setDataList] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+  
+//   async function getDataList() {
+//     try {
+//       const { data } = await Api.get('stats', '?groupName=gender');
+//       setDataList([...Object.values(data.payload)]);
+//       setIsLoading(false);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
 
-  return (
-    <div className="App">
-      <Bar
-        pointStyle="star"
-        data={{
-          labels: data.labels,
-          responsive: true,
-          offset: true,
-          datasets: [
-            {
-              label: "Mobile",
-              pointStyle: "rectRounded",
-              backgroundColor: "#6ED3FF",
-              barThickness: 40,
-              categoryPercentage: 1,
-              data: data.previousDate.dataSet //From API
-            },
-            {
-              label: "Desktop",
-              backgroundColor: "#1497FF",
-              barThickness: 40,
-              categoryPercentage: 1,
-              pointStyle: "triangle",
-              data: data.currentDate.dataSet //From API
-            }
-          ]
-        }}
-        height={220}
-        options={{
-          offsetGridLines: true,
-          drawTicks: true,
-          layout: {
-            padding: {
-              top: 30,
-              right: 40,
-              bottom: 40
-            }
-          },
-          legend: {
-            display: true,
-            position: "right",
-            align: "start",
-            labels: {
-              usePointStyle: true
-            }
-          },
-          responsive: true,
-          maintainAspectRatio: false,
-          scales: {
-            xAxes: [
-              {
-                stacked: true,
-                ticks: {
-                  padding: 5
-                },
-                gridLines: {
-                  display: false
-                }
-              }
-            ],
-            yAxes: [
-              {
-                stacked: false,
-                gridLines: {
-                  drawBorder: false
-                },
-                ticks: {
-                  beginAtZero: true,
-                  maxTicksLimit: 6,
-                  padding: 20,
-                  callback(n) {
-                    if (n < 1e3) return n;
-                    if (n >= 1e3) return +(n / 1e3).toFixed(1) + "K";
-                  }
-                }
-              }
-            ]
-          }
-        }}
-      />
-    </div>
-  );
-}
+//   useEffect(() => {
+//     getDataList();
+//   }, []);
 
-export default HobbyChart;
+//   return (
+//     <ResponsiveBar className="graphBack"
+//         data={dataList}
+//         keys={[
+//           'Female', 
+//           'male'
+//         ]}
+//         indexBy="country"
+//         margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+//         padding={0.3}
+//         valueScale={{ type: 'linear' }}
+//         indexScale={{ type: 'band', round: true }}
+//         colors={{ scheme: 'nivo' }}
+//         defs={[
+//             {
+//                 id: 'dots',
+//                 type: 'patternDots',
+//                 background: 'inherit',
+//                 color: '#38bcb2',
+//                 size: 4,
+//                 padding: 1,
+//                 stagger: true
+//             },
+//             {
+//                 id: 'lines',
+//                 type: 'patternLines',
+//                 background: 'inherit',
+//                 color: '#eed312',
+//                 rotation: -45,
+//                 lineWidth: 6,
+//                 spacing: 10
+//             }
+//         ]}
+//         fill={[
+//             {
+//                 match: {
+//                     id: 'fries'
+//                 },
+//                 id: 'lines'
+//             },
+//             {
+//                 match: {
+//                     id: 'sandwich'
+//                 },
+//                 id: 'lines'
+//             }
+//         ]}
+//         borderColor={{
+//             from: 'color',
+//             modifiers: [
+//                 [
+//                     'darker',
+//                     1.6
+//                 ]
+//             ]
+//         }}
+//         axisTop={null}
+//         axisRight={null}
+//         axisBottom={{
+//             tickSize: 5,
+//             tickPadding: 5,
+//             tickRotation: 0,
+//             legend: 'country',
+//             legendPosition: 'middle',
+//             legendOffset: 32
+//         }}
+//         axisLeft={{
+//             tickSize: 5,
+//             tickPadding: 5,
+//             tickRotation: 0,
+//             legend: 'food',
+//             legendPosition: 'middle',
+//             legendOffset: -40
+//         }}
+//         labelSkipWidth={12}
+//         labelSkipHeight={12}
+//         labelTextColor={{
+//             from: 'color',
+//             modifiers: [
+//                 [
+//                     'darker',
+//                     1.6
+//                 ]
+//             ]
+//         }}
+//         legends={[
+//             {
+//                 dataFrom: 'keys',
+//                 anchor: 'bottom-right',
+//                 direction: 'column',
+//                 justify: false,
+//                 translateX: 120,
+//                 translateY: 0,
+//                 itemsSpacing: 2,
+//                 itemWidth: 100,
+//                 itemHeight: 20,
+//                 itemDirection: 'left-to-right',
+//                 itemOpacity: 0.85,
+//                 symbolSize: 20,
+//                 effects: [
+//                     {
+//                         on: 'hover',
+//                         style: {
+//                             itemOpacity: 1
+//                         }
+//                     }
+//                 ]
+//             }
+//         ]}
+//         role="application"
+//         ariaLabel="Nivo bar chart demo"
+//         barAriaLabel={function(e){return e.id+": "+e.formattedValue+" in country: "+e.indexValue}}
+//     />
+//   );
+// }
+
+
+// export default HobbyChart;
