04cf32b883ddf6eec09d9e3e26aaabc4ae9a6e4a
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-02 04:16:34 +0900

    modify: card

---
+++ front/src/components/common/InputNickname.js
@@ -0,0 +1,76 @@
+import React, { useContext } from "react";
+import Typewriter from "typewriter-effect";
+import styled from "../../css/match.module.css";
+import { useNavigate } from "react-router-dom";
+import "../../css/typingFont.css";
+import usePathParams from "../../utils/usePathParams";
+import BackButton from "./BackButton";
+
+import { NicknameContext } from "../../context/NicknameContext";
+
+function InputNickname() {
+  const { nickname, setNickname } = useContext(NicknameContext);
+  const navigator = useNavigate();
+  const pathname = usePathParams();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      setNickname(nickname);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleCheckUri = () => {
+    if (pathname === "/game") {
+      navigator("/game-intro");
+    } else if (pathname === "/match") {
+      navigator("/match-intro");
+    }
+  };
+
+  const handleChange = (e) => {
+    setNickname(e.target.value);
+    console.log(nickname);
+  };
+
+  return (
+    <div className={styled.Wrapper}>
+      <div
+        className="nav-bar"
+        style={{ position: "fixed", top: "0", left: "0", zIndex: "1" }}
+      >
+        <BackButton content={"메인으로"} />
+      </div>
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .typeString("닉네임을 입력해주세요.")
+            .callFunction(() => {
+              console.log("String typed out!");
+            })
+            .pauseFor(2500)
+            .callFunction(() => {
+              console.log("All strings were deleted");
+            })
+            .start();
+        }}
+      />
+      <form onSubmit={handleSubmit} className={styled.inputForm}>
+        <div className={styled.inputBack}>
+          <input value={nickname} onChange={handleChange} />
+        </div>
+        <button
+          type="button"
+          className={nickname.length > 0 ? styled.btnActive : styled.btnHidden}
+          onClick={handleCheckUri}
+        >
+          확인
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default InputNickname;
