0a43bdd19b8a55fe8b9d74965ab5c6124f70ea3f
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-03 00:39:45 +0900

    modify: add hobby stacked chart

--- front/src/components/Statistics/HobbyChart.js
+++ front/src/components/Statistics/HobbyChart.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import * as Api from "../../api";
-import { Chart, registerables } from "chart.js";
-import { Bar } from 'react-chartjs-2';
-Chart.register(...registerables)
+import { BarChart, Bar, XAxis, YAxis,
+	CartesianGrid, Legend, Tooltip } from 'recharts';
 
 const HobbyChart = () => {
   const [dataList, setDataList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  
   async function getDataList() {
     try {
       const { data } = await Api.get('stats?groupName=hobby');
       setDataList([...Object.values(data.payload)]);
+      console.log(data);
       setIsLoading(false);
     } catch (error) {
       console.error(error);
@@ -22,45 +22,20 @@ const HobbyChart = () => {
     getDataList();
   }, []);
 
-  return (
-    <Bar className="graphBack"
-      data={{
-        labels: dataList[1],
-        datasets: [
-          {
-            label: '# Species',
-            data: dataList[2],
-            backgroundColor: [
-              'rgba(242, 34, 110, 0.2)',
-              'rgba(242, 135, 5, 0.3)',
-              'rgba(242, 226, 5, 0.3)',
-              'rgba(3, 166, 60, 0.3)',
-              'rgba(5, 151, 242, 0.3)',
-              'rgba(134, 5, 240, 0.15)',
-            ],
-            borderColor: [
-              'rgba(242, 34, 110, 1)',
-              'rgba(242, 135, 5, 1)',
-              'rgba(242, 226, 5, 1)',
-              'rgba(3, 166, 60, 1)',
-              'rgba(5, 151, 242, 1)',
-              'rgba(134, 5, 240, 1)',
-            ],
-            borderWidth: 1
-          }
-        ]
-      }}
-      height={500}
-      width={1200}
-      options={{
-        responsive: false,
-        plugins: {
-          legend: false // Hide legend
-        },
-      }}
-    />
-  );
-}
 
+	const data = dataList[1];
+
+	return (
+		<BarChart className="graphBack" width={1200} height={500} data={data} >
+			<CartesianGrid />
+			<XAxis dataKey="name" />
+			<YAxis />
+      <Tooltip />
+      <Legend />
+			<Bar dataKey="x" stackId="a" fill="#F5F197" />
+			<Bar dataKey="y" stackId="a" fill="#BDDBF0" />
+		</BarChart>
+	);
+}
 
 export default HobbyChart;
