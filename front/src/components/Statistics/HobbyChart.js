6b613e877cf1634639ec7e59cfc07bee868b225f
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-02 17:54:54 +0900

    modify: change chart

--- front/src/components/Statistics/HobbyChart.js
+++ front/src/components/Statistics/HobbyChart.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import * as Api from "../../api";
 import { Chart, registerables } from "chart.js";
-import { Pie } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 Chart.register(...registerables)
 
 const HobbyChart = () => {
@@ -23,7 +23,7 @@ const HobbyChart = () => {
   }, []);
 
   return (
-    <Pie className="graphBack"
+    <Bar className="graphBack"
       data={{
         labels: dataList[1],
           datasets: [
@@ -50,7 +50,7 @@ const HobbyChart = () => {
           ]
       }}
       height={500}
-      width={500}
+      width={1200}
       options= {{
         responsive: false,
       }}
--- front/src/components/Statistics/StyleChart.js
+++ front/src/components/Statistics/StyleChart.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import * as Api from "../../api";
 import { Chart, registerables } from "chart.js";
-import { Bar } from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
 Chart.register(...registerables)
 
 const StyleChart = () => {
@@ -23,7 +23,7 @@ const StyleChart = () => {
   }, []);
 
   return (
-    <Bar className="graphBack"
+    <Pie className="graphBack"
       data={{
         labels: dataList[1],
           datasets: [
