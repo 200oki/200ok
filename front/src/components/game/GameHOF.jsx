4ade782ed0506c68b59c0be4efc14e7ca8241eb1
kminzy <kminzy0704@gmail.com>
2022-05-03 05:26:57 +0900

    modify: game hof 작업중 feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/components/game/GameHOF.jsx
+++ front/src/components/game/GameHOF.jsx
@@ -4,6 +4,7 @@ import BackButton from "../common/BackButton";
 import { useStyles } from "../../utils/useStyles";
 import * as Api from "../../api";
 import { Typography } from "@mui/material";
+import styled from "../../css/match.module.css";
 
 const GameHOF = () => {
   const classes = useStyles();
@@ -11,11 +12,6 @@ const GameHOF = () => {
   const typoStyles = {
     fontFamily: "TmoneyRoundWindExtraBold",
     fontSize: "1.5rem",
-  };
-  const titleStyles = {
-    fontFamily: "TmoneyRoundWindExtraBold",
-    fontSize: "1.5rem",
-    display: "flex",
   };
   const getData = async () => {
     try {
@@ -26,32 +22,27 @@ const GameHOF = () => {
       console.log(e);
     }
   };
+
   useEffect(() => {
     getData();
   }, []);
+
   return (
     <div className="gameResultRoot">
-      <div className={classes.quizContent}>
-        <BackButton content="뒤로가기" className={classes.leftArrow} />
+      <div className={classes.navBar}>
+        <BackButton content="뒤로가기" className={classes.leftArrow} destination="explore" />
       </div>
       <div className="rankWrap">
         <div className="imageWrap">
           <img src="images/trophy_rm.png" alt="trophy" />
-          <Typography
-            className="title"
-            sx={typoStyles}
-            variant="body1"
-            textAlign="center"
-          >
-            명예의 전당
-          </Typography>
+          <div className={`hofTitle txt-shine`}>명예의 전당</div>
         </div>
 
         {rank.map((item, idx) => {
           return (
             <div className="rankList" key={idx}>
               <Typography sx={typoStyles} variant="body1" textAlign="center">
-                {idx + 1}st
+                {idx + 1}등
               </Typography>
               <Typography sx={typoStyles} variant="body1" textAlign="center">
                 {item.nickname}
@@ -62,6 +53,15 @@ const GameHOF = () => {
             </div>
           );
         })}
+
+        <form className={styled.commentForm}>
+          <div className={styled.commentBack}>
+            <input type="text" placeholder="댓글을 입력해주세요" required />
+          </div>
+          <button type="submit" className={styled.commentReg}>
+            등록
+          </button>
+        </form>
       </div>
     </div>
   );
