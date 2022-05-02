9569ced0b58c29e787e8fdca34c183cd2a168fdb
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-02 19:23:48 +0900

    feat: add guestbook add form

---
+++ front/src/components/Guestbook/GuestbookAddForm.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useContext } from "react";
+import { Guestbook } from "./Guestbook";
+
+const GuestbookAddForm = () => {
+  return (
+    <div>
+      안녕!
+    </div>
+  );
+}
+
+export default Guestbook;
