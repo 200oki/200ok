ed35b839ccaee6f18762a3ae38d38019dded39f1
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-02 18:53:13 +0900

    modify: add colors

--- front/src/components/Statistics/HobbyChart.js
+++ front/src/components/Statistics/HobbyChart.js
@@ -31,12 +31,20 @@ const HobbyChart = () => {
             label: '# Species',
             data: dataList[2],
             backgroundColor: [
+              'rgba(242, 34, 110, 0.2)',
+              'rgba(242, 135, 5, 0.3)',
               'rgba(242, 226, 5, 0.3)',
+              'rgba(3, 166, 60, 0.3)',
               'rgba(5, 151, 242, 0.3)',
+              'rgba(134, 5, 240, 0.15)',
             ],
             borderColor: [
+              'rgba(242, 34, 110, 1)',
+              'rgba(242, 135, 5, 1)',
               'rgba(242, 226, 5, 1)',
+              'rgba(3, 166, 60, 1)',
               'rgba(5, 151, 242, 1)',
+              'rgba(134, 5, 240, 1)',
             ],
             borderWidth: 1
           }
