1bbe0463dbfecc748395c7c22c0d940bdcfd2bf4
Changm-L <chang6585@naver.com>
2022-05-03 03:08:20 +0900

    modify : modify initial Data Type

--- front/src/context/GameAnswerContext.js
+++ front/src/context/GameAnswerContext.js
@@ -3,7 +3,7 @@ import React, { useState, createContext, useMemo } from "react";
 const GameAnswerContext = createContext();
 
 const GameAnswerProvider = ({ children }) => {
-  const [answer, setAnswer] = useState(0);
+  const [answer, setAnswer] = useState([]);
   const store = useMemo(() => ({ answer, setAnswer }), [answer, setAnswer]);
 
   return (
