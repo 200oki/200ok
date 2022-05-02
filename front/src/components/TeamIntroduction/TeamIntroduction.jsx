0d17400a14489004ff2b77e77240209adf9f4da1
kminzy <kminzy0704@gmail.com>
2022-05-03 01:28:42 +0900

    modify: add temp data feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/components/TeamIntroduction/TeamIntroduction.jsx
+++ front/src/components/TeamIntroduction/TeamIntroduction.jsx
@@ -1,27 +1,53 @@
-import React, { useEffect, useState } from "react";
-import * as Api from "../../api";
+import React from "react";
 import mstyled from "../../css/match.module.css";
 import TeamCard from "../TeamIntroduction/TeamCard";
 import BackButton from "../common/BackButton";
 
 const TeamIntroduction = () => {
-  const [tmp, setTmp] = useState([]);
-
-  const getChar = async () => {
-    try {
-      const { data } = await Api.get(
-        `characters/random?size=6&fields=id%2Cname_ko%2Cimage_photo`
-      );
-      setTmp([...Object.values(data.payload)]);
-    } catch (err) {
-      console.error(err);
-    }
-    return () => {};
-  };
-
-  useEffect(() => {
-    getChar();
-  }, []);
+  const teamInfo = [
+    {
+      position: "Back End",
+      name: "신광천",
+      image: "",
+      character: "",
+      introduction: "따봉광천",
+    },
+    {
+      position: "Back End",
+      name: "안지우",
+      image: "https://acnhcdn.com/latest/NpcBromide/NpcNmlHam01.png",
+      character: "애플",
+      introduction: "따봉지우",
+    },
+    {
+      position: "Back End",
+      name: "윤성준",
+      image: "",
+      character: "",
+      introduction: "따봉성준",
+    },
+    {
+      position: "Front End",
+      name: "권민지",
+      image: "https://acnhcdn.com/latest/NpcBromide/NpcNmlDuk01.png",
+      character: "리처드",
+      introduction: "따봉민지",
+    },
+    {
+      position: "Front End",
+      name: "이창민",
+      image: "https://acnhcdn.com/latest/NpcPoster/NpcSpAlw.png",
+      character: "리사",
+      introduction: "따봉미니",
+    },
+    {
+      position: "Front End",
+      name: "홍지운",
+      image: "",
+      character: "힘드러",
+      introduction: "안힘드러",
+    },
+  ];
 
   return (
     <div className={mstyled.Wrapper}>
@@ -33,8 +59,10 @@ const TeamIntroduction = () => {
       </div>
       <div className="container2">
         <div className="card-grid2">
-          {tmp.map((card, idx) => {
-            return <TeamCard tmp={tmp} card={card} idx={idx} key={idx} />;
+          {teamInfo.map((card, idx) => {
+            return (
+              <TeamCard teamInfo={teamInfo} card={card} idx={idx} key={idx} />
+            );
           })}
         </div>
       </div>
