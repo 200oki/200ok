946f2fcf9dbcb801af8e88be6958a6c4fc8bd173
Changm-L <chang6585@naver.com>
2022-05-02 14:57:45 +0900

    feat : add progress bar style

--- front/src/components/game/GameItem.js
+++ front/src/components/game/GameItem.js
@@ -1,12 +1,56 @@
-import React, { useState, useEffect, useRef } from "react";
-import { testData } from "../../utils/util";
+import React, { useState, useEffect } from "react";
 import { useStyles } from "../../utils/useStyles";
-import Card from "./Card";
 import "../../css/quiz.css";
+import * as Api from "../../api";
+import { shuffle } from "../../utils/shuffle";
+import FlashcardList from "./FlashcardList";
 
-const GameItem = () => {
+const GameItem = ({ tier }) => {
   const [isTwoFlipped, setIsTwoFlipped] = useState(0);
+  const [timer, setTimer] = useState(0);
+  const [cards, setCards] = useState([]);
+  const [value, setValue] = useState([]);
   const classes = useStyles();
+
+  const getCards = async () => {
+    try {
+      let url;
+      if (!tier) {
+        url = "characters/random?size=8&tiers=1&fields=name_ko,image_photo";
+      } else {
+        url = `characters/random?size=16&tiers=${tier}&fields=name_ko,image_photo`;
+      }
+      const { data } = await Api.get(url);
+
+      return data.payload;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getCards().then((data) => {
+      setCards(data);
+      let arr = [];
+      [...data].map((item, idx) => {
+        arr.push(data[idx].name_ko);
+        arr.push(data[idx].image_photo);
+      });
+      setValue(shuffle(arr));
+      console.log("random", arr);
+      console.log("data", data);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (timer >= 100) {
+      return () => clearTimeout(tick);
+    }
+    const tick = setTimeout(() => {
+      setTimer(timer + 1.67);
+    }, 1000);
+  }, [timer]);
+
   useEffect(() => {
     if (isTwoFlipped >= 2) {
       //card 뒤집혀있으면 클릭되면 클래스 달아서
@@ -23,29 +67,16 @@ const GameItem = () => {
     }
   }, [isTwoFlipped]);
 
-  const handleCheckTwo = () => {
-    setIsTwoFlipped((v) => v + 1);
-  };
-
   return (
     <React.Fragment>
       <div className={classes.progressBar}>
-        <div className={classes.inProgressBar}></div>
+        <div
+          style={{ width: `${timer}%` }}
+          className={classes.inProgressBar}
+        ></div>
       </div>
-
-      <div className={classes.cardRoot}>
-        {testData.map((item, idx) => {
-          return (
-            <>
-              <div className={classes.wrapCard} onClick={handleCheckTwo}>
-                <Card name={item.name_ko} />
-              </div>
-              <div className={classes.wrapCard} onClick={handleCheckTwo}>
-                <Card chImage={item.image_photo} />
-              </div>
-            </>
-          );
-        })}
+      <div className="container">
+        <FlashcardList flashcard={value} />
       </div>
     </React.Fragment>
   );
