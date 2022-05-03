41266e52866657bba8933ab0f7c83d2c5399cfb1
YunSeongJun <movomo@gmail.com>
2022-05-03 14:01:12 +0900

    modify: merge csmdata.js into csm.js

--- back/src/db/schemas/csm.js
+++ back/src/db/schemas/csm.js
@@ -1,6 +1,11 @@
+import fs from "fs";
+import path from "path";
+
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const model = mongoose.model;
+
+import characters from "./character.js";
 
 const csmSchema = new Schema({
   id: {
@@ -23,4 +28,32 @@ const csmSchema = new Schema({
 
 const CsmModel = model("csms", csmSchema);
 
-export { CsmModel };
+const __dirname = path.resolve();
+let raw = fs.readFileSync(
+  path.resolve(__dirname, "./src/db/schemas/csmtable.json")
+);
+
+/** `csmdata`는 모든 캐릭터들 간의 유사성 평가 결과를 저장하고 있습니다.
+ *
+ * `csmtable.json`은 캐릭터의 `id`만을 포함하고 있지만
+ * 메모리 상에서는 `character` 필드에 `characters` 데이터 항목을 가리키는
+ * 포인터를 저장하게 됩니다.
+ *
+ * ## 구조 예시
+ * ```js
+ *  {
+ *    [id]: [
+ *      { [id]: char.id, distance: number, character: char }
+ *    ]
+ *  }
+ * ```
+ */
+const csmdata = JSON.parse(raw);
+
+for (const similarityRanks of Object.values(csmdata)) {
+  for (const char of similarityRanks) {
+    char.character = characters.id[char.id];
+  }
+}
+
+export { csmdata, CsmModel };
