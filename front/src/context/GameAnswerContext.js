17db64d42bab5e4741e2527993481e9605888162
Changm-L <chang6585@naver.com>
2022-05-03 17:05:33 +0900

    modify : delete legacy Context

--- front/src/context/GameAnswerContext.js
+++ front/src/context/GameAnswerContext.js
@@ -4,11 +4,8 @@ const GameAnswerContext = createContext();
 
 const GameAnswerProvider = ({ children }) => {
   const [answer, setAnswer] = useState([]);
-  const [isTwoSelected, setIsTwoSelected] = useState(false);
-  const store = useMemo(
-    () => ({ answer, setAnswer, isTwoSelected, setIsTwoSelected }),
-    [answer, setAnswer, isTwoSelected, setIsTwoSelected]
-  );
+
+  const store = useMemo(() => ({ answer, setAnswer }), [answer, setAnswer]);
 
   return (
     <GameAnswerContext.Provider value={store}>
