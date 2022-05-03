b37df24f30bac42ce434417a723b6e4feb46bb0c
hongloans <loans93@gmail.com>
2022-05-03 23:41:28 +0900

    feat: bestfriend 컴포넌트 기본 styled components 작성

--- front/src/components/Bestiary/BestFriends.jsx
+++ front/src/components/Bestiary/BestFriends.jsx
@@ -1,9 +1,29 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 
+const BestFriends = (height, offset) => {
+  const BestFriendWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: ${height}px;
+    width: auto;
+    position: absolute;
+    right: calc(50vw - ${offset}px);
+  `;
+
+  const LabelBf = styled.div`
+    background-color: white;
+  `;
+  const IconBf = styled.img`
+    src: ${(props) => props.src};
+  `;
 
   return (
-
+    <BestFriendWrapper>
+      <LabelBf>최고의 궁합</LabelBf>
+    </BestFriendWrapper>
   );
 };
 export default BestFriends;
