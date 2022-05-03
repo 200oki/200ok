00d065c80a10c66ddd81bf1c4fdc80c517ed4dc2
YunSeongJun <movomo@gmail.com>
2022-05-03 14:23:26 +0900

    docs: blueprint getSimilarCharsOf

--- back/src/services/csmService.js
+++ back/src/services/csmService.js
@@ -31,6 +31,26 @@ class CsmService {
     const total = totalList[0].total;
     return total;
   }
+
+  /** 하드코드된 `csmdata` 테이블에서 유사한 캐릭터의 배열을 읽어 반환합니다.
+   *
+   * @arg {{
+   *  id: string,
+   *  top?: number = 0
+   *  bottom?: number = 0
+   * }} kwargs
+   *    - `top`과 `bottom`은 각각 유사도 상위/하위권에서 자를 숫자입니다.
+   *    - `top`과 `bottom`이 모두 0이면 전체 유사도 배열을 반환합니다.
+   *    - `top` 또는 `bottom` 중 하나가 지정되면 그 부분만 반환합니다.
+   *    - 둘 모두가 지정되면 `[ ...top, ...bottom ]` 형식으로 반환합니다.
+   *    - 어떤 경우에도 순서는 유사도 상위 -> 하위 순입니다.
+   * @return {any[]} - 유사한 캐릭터들이 비슷한 순서로 정렬된 배열을 반환합니다.
+   *    구조는 다음과 같습니다.
+   *    ```js
+   *    [ { id, distance, character: char, }, ]
+   *    ```
+   */
+  static getSimilarCharsOf({ id, top = 0, bottom = 0 }) {}
 }
 
 export { CsmService };
