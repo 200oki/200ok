7ec4569026edab8f6de81dde5a205d240a70ed4a
Changm-L <chang6585@naver.com>
2022-05-03 14:30:24 +0900

    feat : add isTwoSelected Context

--- front/src/context/GameAnswerContext.js
+++ front/src/context/GameAnswerContext.js
@@ -4,7 +4,11 @@ const GameAnswerContext = createContext();
 
 const GameAnswerProvider = ({ children }) => {
   const [answer, setAnswer] = useState([]);
-  const store = useMemo(() => ({ answer, setAnswer }), [answer, setAnswer]);
+  const [isTwoSelected, setIsTwoSelected] = useState(false);
+  const store = useMemo(
+    () => ({ answer, setAnswer, isTwoSelected, setIsTwoSelected }),
+    [answer, setAnswer, isTwoSelected, setIsTwoSelected]
+  );
 
   return (
     <GameAnswerContext.Provider value={store}>
