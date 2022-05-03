974d0bf8cf05395cc01fee60351362cecc2edfb9
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-04 04:10:49 +0900

    modify: add guestbook form

--- front/src/components/Guestbook/AddGuestbookModal.jsx
+++ front/src/components/Guestbook/AddGuestbookModal.jsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
+import { Box, Modal, Typography } from "@mui/material";
+import { useStyles } from "../../utils/useStyles";
+
 import "../../css/GuestPost.css";
 
 const AddGuestbookModal = () => {
+  const classes = useStyles();
 
   return (
     <div className="guestbookPostBg">
-      <h2>제목</h2>
-      <div className="guestbook">
-        <textarea>
-          내용
-        </textarea>
-      </div>
+      <Box className="modalBg">
+        <Typography
+          id="modal-modal-title"
+          variant="h6"
+          className={classes.modalFont}
+        >
+          <textarea>
+            내용을 입력해주세요.
+          </textarea>
+          <button>
+            오케이!
+          </button>
+        </Typography>
+      </Box>
     </div>
   );
 }
