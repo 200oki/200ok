5c5d221936cb049925876ba541dc0e64b1a7a8d4
hongloans <loans93@gmail.com>
2022-05-04 00:26:49 +0900

    style: bf 여러가지 스타일 변경

--- front/src/components/Bestiary/BestFriends.jsx
+++ front/src/components/Bestiary/BestFriends.jsx
@@ -2,38 +2,44 @@ import styled from "styled-components";
 import { useEffect, useState } from "react";
 import * as Api from "../../api";
 
-const BestFriends = ({ height, offset, __id }) => {
-  const BestFriendWrapper = styled.div`
-    display: flex;
-    border: 5px solid black;
-    border-radius: 25px;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: ${height}px;
-    width: auto;
-    position: absolute;
-    right: calc(50vw - ${offset}px);
-  `;
+const BestFriendWrapper = styled.div`
+  display: flex;
+  border: 5px solid black;
+  border-radius: 25px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: ${(props) => props.height}px;
+  width: auto;
+  position: absolute;
+  right: calc(50vw - ${(props) => props.offset}px);
+`;
 
-  const LabelBf = styled.div`
-    background-color: white;
-  `;
-  const IconBf = styled.img`
-    src: ${(props) => props.src};
-  `;
+const LabelBf = styled.div`
+  color: #543627;
+  font-size: 1.2rem;
+`;
+const IconBf = styled.img`
+  src: ${(props) => props.src};
+`;
+
+const BestFriends = ({ __id }) => {
   const [friends, setFriends] = useState([]);
+  const [height, setHeight] = useState(0);
+  const [offset, setOffset] = useState(0);
+
   const getFriends = async () => {
     const { data } = await Api.get(`csmdata/${__id}?top=3&bottom=0`);
     setFriends(data.payload);
   };
   useEffect(() => {
     getFriends();
-    console.log(friends);
+    setHeight(document.getElementById("img-bubble").clientHeight);
+    setOffset(document.getElementById("columnWrapper").clientWidth / 2);
   }, []);
 
   return (
-    <BestFriendWrapper>
+    <BestFriendWrapper height={height} offset={offset}>
       <LabelBf>최고의 궁합</LabelBf>
       {friends.map((friend, idx) => {
         return <IconBf key={`friend-${idx}`} src={friend.character.image_icon} />;
