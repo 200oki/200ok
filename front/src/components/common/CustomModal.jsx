17aa270b0f308b545f1b04555586ad9da8a97c0b
Changm-L <chang6585@naver.com>
2022-05-03 16:59:00 +0900

    feat : add CustomModal

---
+++ front/src/components/common/CustomModal.jsx
@@ -0,0 +1,29 @@
+import { Box, Modal, Typography } from "@mui/material";
+import React from "react";
+
+const CustomModal = ({ children, open, onClose }) => {
+  const modalStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #fff",
+    boxShadow: 24,
+    p: 4,
+    outline: "none",
+  };
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={modalStyle}>{children}</Box>
+    </Modal>
+  );
+};
+
+export default CustomModal;
