93e51ae5744adbdfe80bfda381ad2bf52ffabbd0
YunSeongJun <movomo@gmail.com>
2022-05-04 21:45:49 +0900

    modify: exit arg default value

--- back/src/utils/errors.js
+++ back/src/utils/errors.js
@@ -38,6 +38,8 @@ class RequestError extends Error {
 
 /** 기본 `Error` 타입에 더 구체적인 정보를 담을 수 있도록 확장한 범용 에러입니다. */
 class AppError extends Error {
+  static status = STATUS_500_INTERNALSERVERERROR;
+  static exit = 1;
   /** 더 구체적인 정보를 담을 수 있도록 확장한 범용 에러입니다.
    *
    * @arg {{
@@ -48,7 +50,7 @@ class AppError extends Error {
    * }} kwargs - 일부는 생략 가능합니다. 전부 생략하려면 `{}`를 줍니다.
    *    - `name`: 기본값 `this.constructor.name` = `"AppError"`
    *    - `status`: http status code입니다. 기본값 `500 Internal Server Error`
-   *    - `exit`: 0이 아니면 `exit`에 인자로 들어가 프로세스를 끝냅니다. 기본값 `0`
+   *    - `exit`: 0이 아니면 `exit`에 인자로 들어가 프로세스를 끝냅니다. 기본값 `1`
    *    - `detail`: 에러 분류를 돕기 위한 추가 정보입니다.
    * @arg {string} [message] - `Error` 컨스트럭터로 릴레이할 메시지입니다.
    * @arg {{cause: Error}} [options] - `{ cause?: Error }`
@@ -58,14 +60,13 @@ class AppError extends Error {
   constructor({ name, status, exit, detail }, ...params) {
     super(...params);
     Object.setPrototypeOf(this, new.target.prototype);
-
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, AppError);
     }
 
     this.name = name ?? this.constructor.name;
-    this.status = status ?? STATUS_500_INTERNALSERVERERROR;
-    this.exit = exit ?? 0;
+    this.status = status ?? this.constructor.status;
+    this.exit = exit ?? this.constructor.exit;
     this.detail = detail;
   }
 }
