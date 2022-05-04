8f21a9962553c50a523d60116e12ab2a9102debe
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-05 05:05:04 +0900

    modify: change date

--- front/src/components/Guestbook/GuestbookList.jsx
+++ front/src/components/Guestbook/GuestbookList.jsx
@@ -45,7 +45,7 @@ const GuestbookList = () => {
       if (state.modal) { // 만약 modal이 true 라면 받아온 데이터를 모달로 띄우기
         setModal(state.modal);
         setContent(state.payload.content);
-        setDate(state.payload.createdAt.slice(0, 11));
+        setDate(state.payload.createdAt.slice(0, 10));
       }
     }
   }, [state]);
@@ -74,7 +74,7 @@ const GuestbookList = () => {
   const handleClick = (element) => {
     setModal((v) => !v);
     setContent(element.content);
-    setDate(element.createdAt.slice(0, 11)); // 날짜를 연-월-일로 자름 ex) 2022-05-04
+    setDate(element.createdAt.slice(0, 10)); // 날짜를 연-월-일로 자름 ex) 2022-05-04
   };
 
   const cardPerColumn = 2;
