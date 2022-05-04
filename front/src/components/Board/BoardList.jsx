b553319377e9c64af70ab29ef86b15e77ccaabd6
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-05 04:15:57 +0900

    feat: add board list

---
+++ front/src/components/Board/BoardList.jsx
@@ -0,0 +1,253 @@
+// import { useEffect, useState } from "react";
+// import styled, { keyframes } from "styled-components";
+// import * as Api from "../../api";
+// import { Slider } from "@mui/material";
+// import { styled as Styled } from "@mui/material/styles";
+// import BackButton from "../common/BackButton";
+// import HomeButton from "../common/HomeButton";
+// import { useNavigate } from "react-router-dom";
+
+// const Navigator = styled.div`
+//   position: fixed;
+//   top: 0;
+//   z-index: 1;
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: space-between;
+//   width: 100vw;
+// `;
+
+// const Container = styled.div`
+//   position: relative;
+// &::before {
+//   background-image: url("/images/leafBgImg.png");
+//   content: " ";
+//   display: block;
+//   position: absolute;
+//   left: 0;
+//   top: 0;
+//   width: 100vw;
+//   height: 100vh;
+//   opacity: 0.5;
+//   background-position: 50% 0;
+//   z-index: -100;
+// `;
+// const pop = keyframes`
+//   from {
+//     opacity: 0;
+//   }
+//   to {
+//     opacity: 1;
+//   }
+// `;
+// const Content = styled.div`
+//   position: relative;
+//   height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   animation: ${pop} 1s linear forwards;
+// `;
+
+// const Wrapper = styled.div`
+//   position: relative;
+//   top: 30px;
+//   right: 50px;
+// `;
+
+// const ContentWrapper = styled.div`
+//   width: 85vw;
+//   height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+// const ContentContainer = styled.div`
+//   width: 100%;
+//   height: 70%;
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: flex-start;
+//   align-items: space-around;
+//   overflow: scroll;
+//   overflow-x: hidden;
+//   white-space: nowrap;
+//   &::-webkit-scrollbar {
+//     display: none;
+//   }
+// `;
+
+// const Card = styled.div`
+//   width: 170px;
+//   height: 170px;
+//   border-radius: 25px;
+//   background-color: white;
+//   display: flex;
+//   justify-content: center;
+//   cursor: pointer;
+//   align-items: center;
+//   box-shadow: 1px 2px 2px 0px rgba(0, 0, 0, 0.2);
+//   background-size: cover;
+//   background-image: url(${(props) => props.src});
+// `;
+
+// const Column = styled.div`
+//   margin-right: 70px;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-around;
+// `;
+
+// const Name = styled.div`
+//   background-color: white;
+//   width: 100%;
+//   height: 40px;
+//   border-radius: 0 0 25px 25px;
+//   box-shadow: 1px 2px 2px 0px rgba(0, 0, 0, 0.2);
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   font-family: "TmoneyRoundWindExtraBold";
+//   font-size: 1.24rem;
+//   position: relative;
+//   top: 50%;
+// `;
+
+// const PrettoSlider = Styled(Slider)({
+//   color: "#52af77",
+//   height: 8,
+//   "& .MuiSlider-track": {
+//     border: "none",
+//   },
+//   "& .MuiSlider-thumb": {
+//     height: 24,
+//     width: 24,
+//     backgroundColor: "#fff",
+//     border: "2px solid currentColor",
+//     "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
+//       boxShadow: "inherit",
+//     },
+//     "&:before": {
+//       display: "none",
+//     },
+//   },
+//   "& .MuiSlider-valueLabel": {
+//     lineHeight: 1.2,
+//     fontSize: 12,
+//     background: "unset",
+//     padding: 0,
+//     width: 32,
+//     height: 32,
+//     borderRadius: "50% 50% 50% 0",
+//     backgroundColor: "#52af77",
+//     transformOrigin: "bottom left",
+//     transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
+//     "&:before": { display: "none" },
+//     "&.MuiSlider-valueLabelOpen": {
+//       transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
+//     },
+//     "& > *": {
+//       transform: "rotate(45deg)",
+//     },
+//   },
+// });
+
+// const BoardList = () => {
+//   const navigate = useNavigate();
+
+//   const [villagers, setVillagers] = useState([]);
+//   const [count, setCount] = useState(0);
+//   const [show, setShow] = useState(false);
+
+//   const scrollHandler = (e, val) => {
+//     const element = document.getElementById("content");
+//     const maxScrollLeft = element.scrollWidth - element.clientWidth;
+//     element.scrollLeft = (maxScrollLeft / 100) * val;
+//   };
+
+//   const clickHandler = (e) => {
+//     e.preventDefault();
+//   };
+
+//   useEffect(() => {
+//   }, []);
+
+//   const cardPerColumn = 3;
+//   const columns = [];
+
+//   for (let i = 0; i < parseInt(count / cardPerColumn); i++) {
+//     columns.push(
+//       villagers.slice(cardPerColumn * i, cardPerColumn * (i + 1)).map((villager, idx) => {
+//         return (
+//           <Card key={idx} src={villager.image_photo} onClick={() => navigate(`/detail/${villager.id}`)}>
+//             <Name>{villager.name_ko}</Name>
+//           </Card>
+//         );
+//       })
+//     );
+//   }
+//   const restCards = count % cardPerColumn;
+//   if (restCards > 0) {
+//     columns.push(
+//       villagers.slice(-restCards).map((villager, idx) => {
+//         return (
+//           <Card key={idx} src={villager.image_photo} onClick={() => navigate(`/detail/${villager.id}`)}>
+//             <Name>{villager.name_ko}</Name>
+//           </Card>
+//         );
+//       })
+//     );
+//   }
+//   return (
+//     <Container>
+//       <Navigator>
+//         <BackButton content={"뒤로가기"} destination={"bestiary"} />
+//         <Wrapper>
+//           <HomeButton />
+//         </Wrapper>
+//       </Navigator>
+//       <Content>
+//         <ContentWrapper>
+//           <ContentContainer id="content">
+//             {columns.map((column, idx) => {
+//               return <Column key={idx}>{column}</Column>;
+//             })}
+//           </ContentContainer>
+//           <PrettoSlider onChange={scrollHandler} />
+//         </ContentWrapper>
+//       </Content>
+//     </Container>
+//   );
+// };
+
+// export default BoardList;
+
+import React from "react";
+import UploadFiles from './UploadFiles';
+
+const NoticeWriteComponent = () => {
+
+    const uploadReferenece = React.createRef();
+
+    async function onClickSearch() {
+        await uploadReferenece.current.upload().then(function (result) {
+            const files = result;
+            alert('저장 완료');
+        }).catch(function (err) {
+        });
+    }
+
+    return (
+       <div>
+          <UploadFiles ref={uploadReferenece} />
+          <div className="text-center pd12">
+             <button className="lf-button primary" onClick={onClickSearch}>저장</button>
+          </div>
+      </div>
+    )
+};
+
+export default NoticeWriteComponent;
