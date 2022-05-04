6713e11becdcf5e53d10c99f014bb340c3d943c4
YunSeongJun <movomo@gmail.com>
2022-05-04 21:21:51 +0900

    feat: implement AppError

--- back/src/utils/errors.js
+++ back/src/utils/errors.js
@@ -33,4 +33,38 @@ class RequestError extends Error {
   }
 }
 
+/** 기본 `Error` 타입에 더 구체적인 정보를 담을 수 있도록 확장한 범용 에러입니다. */
+class AppError extends Error {
+  /** 더 구체적인 정보를 담을 수 있도록 확장한 범용 에러입니다.
+   *
+   * @arg {{
+   *  name?: string,
+   *  status?: number,
+   *  exit?: number,
+   *  detail?: any
+   * }} kwargs - 일부는 생략 가능합니다. 전부 생략하려면 `{}`를 줍니다.
+   *    - `name`: 기본값 `this.constructor.name` = `"AppError"`
+   *    - `status`: http status code입니다. 기본값 `400 Bad Request`
+   *    - `exit`: 0이 아니면 `exit`에 인자로 들어가 프로세스를 끝냅니다. 기본값 `0`
+   *    - `detail`: 에러 분류를 돕기 위한 추가 정보입니다.
+   * @arg {string} [message] - `Error` 컨스트럭터로 릴레이할 메시지입니다.
+   * @arg {{cause: Error}} [options] - `{ cause?: Error }`
+   * @arg {string} [fileName]
+   * @arg {number} [lineNumber]
+   */
+  constructor({ name, status, customCode, operational }, ...params) {
+    super(...params);
+    Object.setPrototypeOf(this, new.target.prototype);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, AppError);
+    }
+
+    this.name = name ?? this.constructor.name;
+    this.status = status ?? STATUS_400_BADREQUEST;
+    this.customCode = customCode ?? -1;
+    this.exit = exit ?? 0;
+  }
+}
+
 export { RequestError };
