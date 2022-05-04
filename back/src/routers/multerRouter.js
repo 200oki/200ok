a542e7401f9970c5a1e778bfac68dbb50d66b313
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-05 06:11:46 +0900

    modify: change import

--- back/src/routers/multerRouter.js
+++ back/src/routers/multerRouter.js
@@ -1,4 +1,5 @@
-const multer = require('multer');
+import multer from "multer";
+import { Router } from "express";
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -6,5 +7,14 @@ let storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}_${file.originalname}`);
+  },
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== '.png' || ext !== '.jpg') {
+      return cb(res.status(400).end('only png, jpg are allowed'), false);
+    }
+    cb(null, true);
   }
-})
+});
+
+const upload = multer({ storage: storage }).single("file");
