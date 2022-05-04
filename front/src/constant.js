8d04010b01877ff4d7d5c25b00d12709436b843a
hongloans <loans93@gmail.com>
2022-05-05 01:50:47 +0900

    refact: save info-keys as constant

--- front/src/constant.js
+++ front/src/constant.js
@@ -1,1 +1,13 @@
-export const CharacterInfoKeys = [];
+export const CharacterInfoKeys = {
+  name_ko: "이름",
+  gender: "성별",
+  species: "동물",
+  birthday: "생일",
+  hobby: "취미",
+  personality: "성격",
+  colors: "좋아하는 색",
+  styles: "선호 스타일",
+  tier: "티어",
+  rank: "티어 내 순위",
+  favorite_song: "좋아하는 노래",
+};
