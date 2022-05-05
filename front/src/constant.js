504772e101da3f3e1e1c4de227f1475d3ac9bc32
hongloans <loans93@gmail.com>
2022-05-06 00:48:43 +0900

    feat: new constant - month-days

--- front/src/constant.js
+++ front/src/constant.js
@@ -11,4 +11,18 @@ const CharacterInfoKeys = {
   rank: "티어 내 순위",
   favorite_song: "좋아하는 노래",
 };
-export default CharacterInfoKeys;
+const MonthDays = {
+  1: 31,
+  2: 29,
+  3: 31,
+  4: 30,
+  5: 31,
+  6: 30,
+  7: 31,
+  8: 31,
+  9: 30,
+  10: 31,
+  11: 30,
+  12: 31
+}
+export { CharacterInfoKeys, MonthDays };
