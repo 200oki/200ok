21edb9e7cbe15bb2dc5c4b13be9d639b952c0cac
kminzy <kminzy0704@gmail.com>
2022-05-05 22:36:20 +0900

    modify: change personality data feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/components/match/SelectPersonality.jsx
+++ front/src/components/match/SelectPersonality.jsx
@@ -10,14 +10,46 @@ function SelectPersonality({ nextSlide }) {
   const [personality, setPersonality] = useState(null);
 
   const personalities = [
-    { key: "무뚝뚝", value: "츤데레라는 소리를 자주 듣는 편이다." },
-    { key: "아이돌", value: "엉뚱하고 오덕기질이 있다." },
-    { key: "단순 활발", value: "노빠꾸 기질이 있는 편이다." },
-    { key: "먹보", value: "주변의 변화를 늦게 알아차리는 편이다." },
-    { key: "친절함", value: "평소에 남을 잘 챙기는 편이다." },
-    { key: "성숙함", value: "쇼핑, 패션에 관심이 많다." },
-    { key: "운동광", value: "운동을 즐겨하는 편이다." },
-    { key: "느끼함", value: "내 안의 흑염룡이 날뛰고 있는 것 같다. 크큭." },
+    {
+      key: "무뚝뚝",
+      value: "츤데레라는 소리를 자주 듣는 편이다.",
+      result: "겉으로는 강해보여도 의외로 여린면이 있는",
+    },
+    {
+      key: "아이돌",
+      value: "엉뚱하고 오덕기질이 있다.",
+      result: "사교적이고 발랄한",
+    },
+    {
+      key: "단순 활발",
+      value: "노빠꾸 기질이 있는 편이다.",
+      result: "거침없고 털털한",
+    },
+    {
+      key: "먹보",
+      value: "주변의 변화를 늦게 알아차리는 편이다.",
+      result: "순수한 어린 아이같은",
+    },
+    {
+      key: "친절함",
+      value: "평소에 남을 잘 챙기는 편이다.",
+      result: "누구에게든 친절하고 상냥한",
+    },
+    {
+      key: "성숙함",
+      value: "쇼핑, 패션에 관심이 많다.",
+      result: "우아하고 기품있는",
+    },
+    {
+      key: "운동광",
+      value: "운동을 즐겨하는 편이다.",
+      result: "활기찬 근육덕후인",
+    },
+    {
+      key: "느끼함",
+      value: "내 안의 흑염룡이 날뛰고 있는 것 같다. 크큭.",
+      result: "낭만적이고 로맨틱한",
+    },
   ];
 
   const handlePersonalityChange = (e) => {
