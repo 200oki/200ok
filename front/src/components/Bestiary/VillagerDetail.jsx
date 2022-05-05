89f47efb19e534bfa49421511e6656e975d97b28
hongloans <loans93@gmail.com>
2022-05-05 17:35:20 +0900

    refact : move styled components below return

--- front/src/components/Bestiary/VillagerDetail.jsx
+++ front/src/components/Bestiary/VillagerDetail.jsx
@@ -8,54 +8,6 @@ import SpeechBubble from "./SpeechBubble";
 import BestFriends from "./BestFriends";
 import Info from "./Info";
 
-const Navigator = styled.div`
-  position: fixed;
-  top: 0;
-  z-index: 1;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 100vw;
-`;
-const Wrapper = styled.div`
-  position: relative;
-  top: 30px;
-  right: 50px;
-`;
-
-const Container = styled.div`
-  position: relative;
-&::before {
-  background-image: url("/images/leafBgImg.png");
-  content: " ";
-  display: block;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100vw;
-  height: 100vh;
-  opacity: 0.5;
-  background-position: 50% 0;
-  z-index: -100;
-`;
-const pop = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-`;
-const Content = styled.div`
-  position: relative;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  animation: ${pop} 1s linear forwards;
-`;
-
 const VillagerDetail = () => {
   const [villager, setVillager] = useState(null);
   const [friends, setFriends] = useState(null);
@@ -107,4 +59,51 @@ const VillagerDetail = () => {
     </Container>
   );
 };
+const Navigator = styled.div`
+  position: fixed;
+  top: 0;
+  z-index: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100vw;
+`;
+const Wrapper = styled.div`
+  position: relative;
+  top: 30px;
+  right: 50px;
+`;
+
+const Container = styled.div`
+  position: relative;
+&::before {
+  background-image: url("/images/leafBgImg.png");
+  content: " ";
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  opacity: 0.5;
+  background-position: 50% 0;
+  z-index: -100;
+`;
+const pop = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+const Content = styled.div`
+  position: relative;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  animation: ${pop} 1s linear forwards;
+`;
 export default VillagerDetail;
