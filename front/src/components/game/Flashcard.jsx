09c3d5f3ad1716bd782468f3a92aff996ebfbd53
Changm-L <chang6585@naver.com>
2022-05-05 14:37:09 +0900

    modify : add FlashCard comment

--- front/src/components/game/Flashcard.jsx
+++ front/src/components/game/Flashcard.jsx
@@ -5,6 +5,7 @@ import { GameContext } from "../../context/GameContext";
  * @param flashcard
  * card data
  * @param handleChoice
+ * choiceOne, choiceTwo
  * @param isTwoSelected
  * @param setIsTwoSelected
  */
