7482ba9f162a9909a92fa83bd7efe1ce7f7023c4
hongloans <loans93@gmail.com>
2022-05-05 17:33:30 +0900

    modify: 코드내 컴포넌트 순서변경

--- front/src/components/Bestiary/Info.jsx
+++ front/src/components/Bestiary/Info.jsx
@@ -2,6 +2,6 @@ import NormalInfo from "./NormalInfo";
 import SpecialInfo from "./SpecialInfo";
 
 const Info = ({ villager }) => {
-  return <div>{!villager.special ? <NormalInfo villager={villager} /> : <SpecialInfo villager={villager} />}</div>;
+  return <div>{villager.special ? <SpecialInfo villager={villager} /> : <NormalInfo villager={villager} />}</div>;
 };
 export default Info;
