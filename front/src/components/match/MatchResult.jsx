9b65eec757205e9376c5dbe7f0ee9497894b5849
kminzy <kminzy0704@gmail.com>
2022-05-05 19:08:50 +0900

    modify: add seperated component feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/components/match/MatchResult.jsx
+++ front/src/components/match/MatchResult.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useRef, useEffect, useState } from "react";
 import * as Api from "../../api";
 import { useNavigate } from "react-router-dom";
 import styled from "../../css/match.module.css";
+import MatchResultMyChar from "./MatchResultMyChar";
 import MatchResultCompat from "./MatchResultCompat";
 import MatchResultRank from "./MatchResultRank";
 import MatchResultComment from "./MatchResultComment";
@@ -194,34 +195,11 @@ function MatchResult() {
         <BackButton content={"메인으로"} destination={"explore"} />
       </div>
       <div className={styled.inner}>
-        <div className={styled.imgWrapper}>
-          <img src={myChar.image_photo} alt={"주민 사진"} />
-        </div>
-        <div className={styled.textWrapper}>
-          <div>{nickname} 님과 잘 어울리는 주민은</div>
-          <div className={styled.villagerName}>
-            ❝{" "}
-            <span style={{ color: "#FA8D74", textShadow: "2px 2px #FFC4A8" }}>
-              {myChar.name_ko}
-            </span>{" "}
-            ❞
-          </div>
-          <div>귀염뽀짝 어쩌구 저쩌구</div>
-          <div>구구절절 쫑알쫑알</div>
-          <div>최고의 궁합!</div>
-          <div className={styled.avgText}>
-            (전체 참여자 중{" "}
-            <span className={styled.avgSubText}>{myChar.avg}%</span> 가 같은
-            주민이 나왔어요!)
-          </div>
-        </div>
-        <div className={styled.btnsWrapper}>
-          <button>공유하기</button>
-          <button onClick={goToFirstPage}>다시하기</button>
-          <button onClick={goToPosition}>유형별 궁합</button>
-          <button onClick={goToPosition}>가장 많은 유형</button>
-          <button onClick={goToPosition}>반응 남기기</button>
-        </div>
+        <MatchResultMyChar
+          myChar={myChar}
+          goToPosition={goToPosition}
+          goToFirstPage={goToFirstPage}
+        />
       </div>
       <div className={styled.inner}>
         <MatchResultCompat goodBad={goodBad} goToPosition={goToPosition} />
