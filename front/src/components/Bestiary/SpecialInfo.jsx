926f57120e40b06086d87cb13922c24a542e2ac1
hongloans <loans93@gmail.com>
2022-05-05 14:40:58 +0900

    refact: special villager label styled component

--- front/src/components/Bestiary/SpecialInfo.jsx
+++ front/src/components/Bestiary/SpecialInfo.jsx
@@ -5,20 +5,38 @@ import Details from "./Details";
 
 const SpecialInfo = ({ villager }) => {
   return (
-    <Row>
-      <Details label={CharacterInfoKeys.gender} value={villager.gender} />
-      <Details label={CharacterInfoKeys.birthday} value={villager.birthday} />
-    </Row>
+    <>
+      <Special>스페셜 주민 입니다.</Special>
+      <Row>
+        <Details label={CharacterInfoKeys.gender} value={villager.gender} />
+        <Details label={CharacterInfoKeys.birthday} value={villager.birthday} />
+      </Row>
+    </>
   );
 };
 
 const Row = styled.div`
-  height: 30vh;
   width: auto;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
+  margin-top: 50px;
+`;
+
+const Special = styled.div`
+  background-color: #5e314b;
+  width: 300px;
+  color: white;
+  height: 40px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "TmoneyRoundWindExtraBold";
+  font-size: 1.24rem;
+  box-shadow: 1px 2px 2px 0px rgba(0, 0, 0, 0.2);
+  margin-top: 50px;
 `;
 
 export default SpecialInfo;
