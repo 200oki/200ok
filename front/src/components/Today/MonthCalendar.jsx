6eec919ec819bc4f5e00d36026a55b20dd3ef699
hongloans <loans93@gmail.com>
2022-05-05 23:46:31 +0900

    feat : get villagers by birthday_month function

--- front/src/components/Today/MonthCalendar.jsx
+++ front/src/components/Today/MonthCalendar.jsx
@@ -11,6 +11,18 @@ const Container = styled.div`
 
 const MonthCalendar = ({ month }) => {
   const [villagers, setVillagers] = React.useState([]);
+  const fieldsToGet = ["id", "name_ko", "image_icon", "birthday_day"];
+  React.useEffect(() => {
+    const getVillagers = async () => {
+      try {
+        const { data } = await Api.get(`characters/search?fields=${fieldsToGet.join(",")}&props=birthday_month&values=${month}`);
+        setVillagers(data.payload);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getVillagers();
+  }, []);
   return <Container />;
 };
 
