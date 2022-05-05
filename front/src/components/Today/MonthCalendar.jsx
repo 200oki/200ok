77c3bb216db051be407a144d44484b547af9ea6c
hongloans <loans93@gmail.com>
2022-05-05 23:38:03 +0900

    feat: month-calendar component base code

---
+++ front/src/components/Today/MonthCalendar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin-top: 20px;
+  height: 40vw;
+  width: 40vw;
+  background-color: white;
+`;
+
+const MonthCalendar = ({ month }) => {
+  return <Container />;
+};
+
+export default MonthCalendar;
