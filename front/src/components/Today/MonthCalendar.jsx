75efafb1334c9eddb4d90e925d426ba3d518a4d9
hongloans <loans93@gmail.com>
2022-05-06 00:50:29 +0900

    feat: sort villagers by date of birth

--- front/src/components/Today/MonthCalendar.jsx
+++ front/src/components/Today/MonthCalendar.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import * as Api from "../../api";
+import * as Constant from "../../constant";
 
 const Container = styled.div`
   margin-top: 20px;
-  height: 40vw;
-  width: 40vw;
-  background-color: white;
+  height: 900px;
+  width: 1200px;
+  background-color: black;
+`;
+const CharacterDot = styled.img`
+  src: ${(props) => props.src};
+  width: 60px;
+  height: 60px;
 `;
 
 const MonthCalendar = ({ month }) => {
-  const [villagers, setVillagers] = React.useState([]);
+  const [villagers, setVillagers] = React.useState(null);
   const fieldsToGet = ["id", "name_ko", "image_icon", "birthday_day"];
   React.useEffect(() => {
     const getVillagers = async () => {
@@ -22,8 +28,21 @@ const MonthCalendar = ({ month }) => {
       }
     };
     getVillagers();
-  }, []);
-  return <Container />;
+  }, [month]);
+  const days = [];
+  if (villagers) {
+    villagers.map((villager) => {
+      days[parseInt(villager.birthday_day) - 1] = [];
+      days[parseInt(villager.birthday_day) - 1].push(villager);
+    });
+  }
+  return (
+    <Container>
+      {villagers?.map((villager, index) => {
+        return <CharacterDot key={`villager-${index}`} src={villager.image_icon} alt={villager.id} />;
+      })}
+    </Container>
+  );
 };
 
 export default MonthCalendar;
