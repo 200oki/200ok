f183dbd27bc073b258113aee0296cbc58e852815
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-06 16:08:43 +0900

    modify: add comment

--- front/src/components/Statistics/GenderChart.js
+++ front/src/components/Statistics/GenderChart.js
@@ -22,31 +22,38 @@ const SpeciesChart = () => {
   }, []);
 
   return (
-    <Pie className="graphBack"
-      data={{
-        labels: dataList[1],
-        datasets: [
-          {
-            label: '# Species',
-            data: dataList[2],
-            backgroundColor: [
-              'rgba(242, 34, 110, 0.2)',
-              'rgba(5, 151, 242, 0.3)',
-            ],
-            borderColor: [
-              'rgba(242, 34, 110, 1)',
-              'rgba(5, 151, 242, 1)',
-            ],
-            borderWidth: 1
-          }
-        ]
-      }}
-      height={500}
-      width={500}
-      options={{
-        responsive: false,
-      }}
-    />
+    <div>
+      <Pie className="graphBack"
+        data={{
+          labels: dataList[1],
+          datasets: [
+            {
+              label: '# Species',
+              data: dataList[2],
+              backgroundColor: [
+                'rgba(242, 34, 110, 0.2)',
+                'rgba(5, 151, 242, 0.3)',
+              ],
+              borderColor: [
+                'rgba(242, 34, 110, 1)',
+                'rgba(5, 151, 242, 1)',
+              ],
+              borderWidth: 1
+            }
+          ]
+        }}
+        height={500}
+        width={500}
+        options={{
+          responsive: false,
+        }}
+      />
+      <div> 
+        <p>동물의 숲에서는 성별마다 성격이 존재합니다.</p>
+        <p>남성은 느끼함, 먹보, 무뚝뚝, 운동광 중 하나입니다.</p>
+        <p>여성은 단순 활발, 성숙함, 아이돌, 친절함 중 하나입니다.</p>
+      </div>
+    </div>
   );
 }
 
