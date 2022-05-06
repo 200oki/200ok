f43734b4545fbc78a88aa4b27b66e851877090c9
Changm-L <chang6585@naver.com>
2022-05-06 22:23:23 +0900

    modify : modify add post logic

--- front/src/components/Board/AddBoard.jsx
+++ front/src/components/Board/AddBoard.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import * as Api from "../../api";
 import "../../css/Board.css";
+import { BoardPostIdContext } from "../../context/BoardPostId";
 
 const AddBoard = () => {
   const navigate = useNavigate();
@@ -10,19 +11,33 @@ const AddBoard = () => {
   const [nickname, setNickname] = useState("");
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [postfiles, setPostfiles] = useState({
-    file: [],
-  });
+  const [postFiles, setPostFiles] = useState([]);
+  const { setPostId } = useContext(BoardPostIdContext);
+
+  const fileHandler = (e) => {
+    if (e.target.files.length > 1) {
+      // setPostFiles((v) => {
+      //   return [[...e.target.files].map((item) => item[0])];
+      // });
+      [...e.target.files].map((item) => {
+        console.log("mapTest======>", item);
+        setPostFiles((v) => [...v, item]);
+      });
+    } else {
+      console.log("one fileHandle", e.target.files);
+      setPostFiles(e.target.files[0]);
+    }
+  };
 
   const handleNicknameChange = (e) => {
     setIsTyping(true);
     setNickname(e.target.value);
-  }
+  };
 
   const handleTitleChange = (e) => {
     setIsTyping(true);
     setTitle(e.target.value);
-  }
+  };
 
   const handleContentChange = (e) => {
     setIsTyping(true);
@@ -32,71 +47,62 @@ const AddBoard = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     e.persist();
+    if (content && title) {
+      const formData = new FormData();
 
-    let files = e.target.profile_files.files;
-    let formData = new FormData();
-    for (let i = 0; i < files.length; i++) {
-      formData.append("files", files[i]);
-    }
+      postFiles.map((item) => formData.append("images", item));
+      formData.append("title", title);
+      formData.append("content", content);
+      formData.append("nickname", nickname);
 
-    let dataSet = {
-      nickname,
-      title,
-      content,
-    };
-
-    formData.append("data", JSON.stringify(dataSet));
-    
-    try {
-      const response = await axios({
-        method: "POST",
-        url: "posts",
-        mode: "cors",
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        data: formData,
-      });
-      setContent((current) => {
-        const newBoard = [...current];
-        newBoard.push(response.data.payload);
-        console.log("새거 :", newBoard.id); // 데이터 잘 들어있는지 확인
-
-        navigate(`/board/${newBoard.id}`);
-      });
-      setIsTyping(false);
-      console.log(response);
-    } catch (err) {
-      console.error(err);
+      console.log("formData=====>", formData.get("images"));
+      try {
+        const response = await axios.post(
+          "http://localhost:5001/posts",
+          formData,
+          {
+            headers: {
+              // "Content-Type": "multipart/form-data",
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        );
+        setPostId(response.data.payload.id);
+        navigate("/board");
+      } catch (err) {
+        console.error(err);
+      }
     }
   };
 
   return (
     <div className="boardPost">
-      <form className="boardForm" onSubmit={(e) => onSubmit(e)}>
+      <form className="boardForm" onSubmit={onSubmit}>
         <div className="contentBack">
           <div className="input">
-            <input 
-              value={nickname} 
-              onChange={handleNicknameChange} 
-              placeholder="닉네임을 입력해주세요" 
-              className="inputNickname" 
+            <input
+              value={nickname}
+              onChange={handleNicknameChange}
+              placeholder="닉네임을 입력해주세요"
+              className="inputNickname"
             />
-            <hr style={{margin: '0 0 0 0'}}/>
-            <input 
-              value={title} 
-              onChange={handleTitleChange} 
+            <hr style={{ margin: "0 0 0 0" }} />
+            <input
+              value={title}
+              onChange={handleTitleChange}
               placeholder="제목을 입력해주세요"
-              className="inputTitle" 
+              className="inputTitle"
             />
             <input
               type="file"
-              name="profile_files"
-              multiple="multiple"
+              name="input_files"
+              onChange={fileHandler}
+              multiple
               className="inputFiles"
             />
           </div>
-          <textarea className="textarea"
+          <textarea
+            className="boardTextarea"
             type="text"
             placeholder="내용을 입력해주세요"
             value={isTyping ? content : ""}
@@ -105,7 +111,11 @@ const AddBoard = () => {
           />
         </div>
         <div className="alignButton">
-          <button type="submit" className="exitBtn" onClick={() => navigate('/board')}>
+          <button
+            type="submit"
+            className="exitBtn"
+            onClick={() => navigate("/board")}
+          >
             그만 쓸래
           </button>
           <button type="submit" className="submitBtn">
@@ -115,6 +125,6 @@ const AddBoard = () => {
       </form>
     </div>
   );
-}
+};
 
 export default AddBoard;
