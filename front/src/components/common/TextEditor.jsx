c51c4c94025c44dac276cc8a1ed7f09fc1986597
Changm-L <chang6585@naver.com>
2022-05-07 03:29:14 +0900

    feat : add WYSIWYG Component

---
+++ front/src/components/common/TextEditor.jsx
@@ -0,0 +1,36 @@
+import Editor from "@draft-js-plugins/editor";
+import createTextAlignmentPlugin from "@draft-js-plugins/text-alignment";
+import createStaticToolbarPlugin from "@draft-js-plugins/static-toolbar";
+import "@draft-js-plugins/text-alignment/lib/plugin.css";
+import "@draft-js-plugins/static-toolbar/lib/plugin.css";
+
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+} from "@draft-js-plugins/buttons";
+import React from "react";
+
+const textAlignmentPlugin = createTextAlignmentPlugin();
+const staticToolbarPlugin = createStaticToolbarPlugin();
+
+const { Toolbar } = staticToolbarPlugin;
+const plugins = [staticToolbarPlugin, textAlignmentPlugin];
+
+const TextEditor = ({ editorState, onChange, placeholder }) => (
+  <div>
+    <Toolbar>
+      {(externalProps) => (
+        <div>
+          <ItalicButton {...externalProps} />
+          <BoldButton {...externalProps} />
+          <UnderlineButton {...externalProps} />
+          <textAlignmentPlugin.TextAlignment {...externalProps} />
+        </div>
+      )}
+    </Toolbar>
+    <Editor editorState={editorState} onChange={onChange} plugins={plugins} />
+  </div>
+);
+
+export default TextEditor;
