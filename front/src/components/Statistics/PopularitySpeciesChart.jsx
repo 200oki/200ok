f0f3342ed9a6f381ba89efb77226c7ca3d5e73f4
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-06 19:00:36 +0900

    feat: popularity by species chart

---
+++ front/src/components/Statistics/PopularitySpeciesChart.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { HeatMapGrid } from 'react-grid-heatmap'
+
+const xLabels = new Array(24).fill(0).map((_, i) => `${i}`)
+const yLabels = ['1', '2', '3', '4', '5', '6']
+const data = new Array(yLabels.length)
+  .fill(0)
+  .map(() =>
+    new Array(xLabels.length)
+      .fill(0)
+      .map(() => Math.floor(Math.random() * 50 + 50))
+  )
+
+const SpeciesTierChart = () => {
+  return (
+    <div
+      style={{
+        width: '100%'
+      }}
+    >
+      <HeatMapGrid
+        data={data}
+        xLabels={xLabels}
+        yLabels={yLabels}
+        // Reder cell with tooltip
+        cellRender={(x, y, value) => (
+          <div title={`Pos(${x}, ${y}) = ${value}`}>{value}</div>
+        )}
+        xLabelsStyle={(index) => ({
+          color: index % 2 ? 'transparent' : '#777',
+          fontSize: '.8rem'
+        })}
+        yLabelsStyle={() => ({
+          fontSize: '.7rem',
+          textTransform: 'uppercase',
+          color: '#777'
+        })}
+        cellStyle={(_x, _y, ratio) => ({
+          background: `rgb(12, 160, 44, ${ratio})`,
+          fontSize: '.8rem',
+          color: `rgb(0, 0, 0, ${ratio / 2 + 0.4})`
+        })}
+        cellHeight='2rem'
+        xLabelsPos='bottom'
+        onClick={(x, y) => alert(`Clicked (${x}, ${y})`)}
+        yLabelsPos='right'
+        square
+      />
+    </div>
+  )
+}
+
+export default SpeciesTierChart;
