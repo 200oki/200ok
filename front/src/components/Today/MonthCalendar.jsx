d3b29409a0fb59bddd5290b4fe63ada6e36ac563
hongloans <loans93@gmail.com>
2022-05-06 17:47:44 +0900

    feat: 달력 일단 성공

--- front/src/components/Today/MonthCalendar.jsx
+++ front/src/components/Today/MonthCalendar.jsx
@@ -6,14 +6,12 @@ import * as Constant from "../../constant";
 
 const MonthCalendar = () => {
   const navigate = useNavigate();
+  const [year, setYear] = React.useState(2022);
+  const [tempYear, setTempYear] = React.useState(year);
   const [month, setMonth] = React.useState(1);
-  const [villagers, setVillagers] = React.useState(null);
-  const [isLoading, setIsLoading] = React.useState(true);
   const fieldsToGet = ["id", "name_ko", "image_icon", "birthday_day"];
-  const year = 2022;
-  const days = [];
-  const weeks = [[], [], [], [], [], []];
   const [stateWeek, setStateWeek] = React.useState(null);
+
   // offset weekday month by month
   const firstDayofMonth = new Date(`${year}-${month}-01`);
   const offset = firstDayofMonth.getDay();
@@ -37,55 +35,75 @@ const MonthCalendar = () => {
   const getVillagers = async () => {
     try {
       const { data } = await Api.get(`characters/search?fields=${fieldsToGet.join(",")}&props=birthday_month&values=${month}`);
-      setVillagers(data.payload);
+      return data.payload;
     } catch (error) {
       console.error(error);
     }
   };
-  const parseVillagers = () => {
-    villagers.map((villager) => {
-      if (Array.isArray(days[parseInt(villager.birthday_day) - 1])) {
-        days[parseInt(villager.birthday_day) - 1].push(villager);
-      } else {
-        days[parseInt(villager.birthday_day) - 1] = [];
-        days[parseInt(villager.birthday_day) - 1].push(villager);
-      }
-    });
 
-    // empty date (birthday no one)
-    for (let i = 0; i < Constant.MonthDays[month]; i++) {
-      if (!!!days[i]) {
-        days[i] = [];
-      }
+  const enterHandler = (e) => {
+    if (e.key === "Enter") {
+      setYear(e.target.value);
     }
-
-    for (let i = 0; i < offset; i++) {
-      days.unshift("offset");
-    }
-
-    days.forEach((day, i) => {
-      weeks[parseInt(i / 7)][i % 7] = day;
-    });
-    setStateWeek(weeks);
-    setIsLoading(false);
   };
 
   React.useEffect(() => {
-    getVillagers();
-  }, [month]);
+    getVillagers()
+      .then((res) => {
+        const days = [];
+        res.map((villager) => {
+          if (Array.isArray(days[parseInt(villager.birthday_day) - 1])) {
+            days[parseInt(villager.birthday_day) - 1].push(villager);
+          } else {
+            days[parseInt(villager.birthday_day) - 1] = [];
+            days[parseInt(villager.birthday_day) - 1].push(villager);
+          }
+        });
+        return days;
+      })
+      .then((days) => {
+        for (let i = 0; i < Constant.MonthDays[month]; i++) {
+          if (!!!days[i]) {
+            days[i] = [];
+          }
+        }
+        return days;
+      })
+      .then((days) => {
+        // offset weekday month by month
+        const firstDayofMonth = new Date(`${year}-${month}-01`);
+        const offset = firstDayofMonth.getDay();
 
-  React.useEffect(() => {
-    if (villagers) {
-      parseVillagers();
-    }
-  }, [month]);
+        for (let i = 0; i < offset; i++) {
+          days.unshift("offset");
+        }
+        return days;
+      })
+      .then((days) => {
+        const weeks = [[], [], [], [], [], []];
+        days.forEach((day, i) => {
+          weeks[parseInt(i / 7)][i % 7] = day;
+        });
+        return weeks;
+      })
+      .then((weeks) => {
+        setStateWeek(weeks);
+      });
+  }, [year, month]);
 
   return (
     <table>
       <thead>
         <tr align="center">
           <th colSpan="7">
-            <p>{year}년</p>
+            <Input
+              value={tempYear}
+              onChange={(e) => {
+                setTempYear(e.target.value);
+              }}
+              onKeyUp={enterHandler}
+            />
+            <span style={{ cursor: "default" }}>년</span>
             <DateNavigator>
               <img
                 src="/images/triangle.png"
@@ -93,8 +111,8 @@ const MonthCalendar = () => {
                 style={{ width: "2rem", height: "1.5rem", transform: "rotate(-90deg)", cursor: "pointer" }}
                 onClick={handleClick}
               />
-              <div style={{ fontSize: "2rem", lineHeight: "1.5rem", width: "1.5rem" }}>{month}</div>
-              <span>월</span>
+              <div style={{ fontSize: "2rem", lineHeight: "1.5rem", width: "1.5rem", cursor: "default" }}>{month}</div>
+              <span style={{ cursor: "default" }}>월</span>
               <img
                 src="/images/triangle.png"
                 alt="right"
@@ -107,15 +125,15 @@ const MonthCalendar = () => {
       </thead>
       <tbody>
         <tr align="center">
-          <td style={{ color: "red" }}>Sun</td>
-          <td>Mon</td>
-          <td>Tue</td>
-          <td>Wed</td>
-          <td>Thu</td>
-          <td>Fri</td>
-          <td style={{ color: "blue" }}>Sat</td>
+          <Weekday style={{ color: "red" }}>Sun</Weekday>
+          <Weekday>Mon</Weekday>
+          <Weekday>Tue</Weekday>
+          <Weekday>Wed</Weekday>
+          <Weekday>Thu</Weekday>
+          <Weekday>Fri</Weekday>
+          <Weekday style={{ color: "blue" }}>Sat</Weekday>
         </tr>
-        {!isLoading &&
+        {stateWeek &&
           stateWeek.map((week, index) => {
             const weekNumber = index;
             return (
@@ -123,11 +141,7 @@ const MonthCalendar = () => {
                 {week.map((day, index) => {
                   const dayNumber = index;
                   if (day === "offset") {
-                    return (
-                      <Day key={`key - ${index}`}>
-                        <span></span>
-                      </Day>
-                    );
+                    return <Day key={`key - ${index}`} offset={true}></Day>;
                   } else if (day.length === 0) {
                     return <Day key={`key - ${index}`}>{weekNumber * 7 + dayNumber + 1 - offset}</Day>;
                   } else {
@@ -168,10 +182,21 @@ const CharacterDot = styled.img`
 const Day = styled.td`
   width: 150px;
   height: 70px;
-  cursor: pointer;
-
+  cursor: ${(props) => (props.offset ? "default" : "pointer")};
   &:hover {
     opacity: 0.6;
   }
+`;
+const Weekday = styled.td`
+  height: 30px;
+  cursor: default;
+`;
+const Input = styled.input`
+  width: 4rem;
+  background: none;
+  border: none;
+  font-size: 1.3rem;
+  text-align: center;
+  color: Grey;
 `;
 export default MonthCalendar;
