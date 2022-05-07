0974d9182302317bdf36a037fcdda517aae2c3d8
kminzy <kminzy0704@gmail.com>
2022-05-08 01:39:19 +0900

    modify: apply share format feat : 새로운 기능 추가 fix : 버그 수정 docs : 문서 수정 modify : 기능 수정 및 코드 수정 refact : 리팩토링

--- front/src/components/match/MatchResultMyChar.jsx
+++ front/src/components/match/MatchResultMyChar.jsx
@@ -13,6 +13,7 @@ const MatchResultMyChar = ({
   goToFirstPage,
   setCopied,
   value,
+  encodedData,
 }) => {
   const { nickname } = useContext(NicknameContext);
   const { resultMent } = useContext(MatchElementContext);
@@ -23,7 +24,11 @@ const MatchResultMyChar = ({
         <img src={myChar.image_photo} alt={"주민 사진"} />
       </div>
       <div className={styled.textWrapper}>
-        <div>{nickname} 님과 잘 어울리는 주민은</div>
+        {encodedData ? (
+          <div>{nickname} 님과 잘 어울리는 주민은</div>
+        ) : (
+          <div>상대방과 잘 어울리는 주민은</div>
+        )}
         <div className={styled.villagerName}>
           ❝{" "}
           <span style={{ color: "#FA8D74", textShadow: "2px 2px #FFC4A8" }}>
@@ -31,24 +36,32 @@ const MatchResultMyChar = ({
           </span>{" "}
           ❞
         </div>
-        <div>{resultMent[0]} 당신은</div>
+        {encodedData ? <div>{resultMent[0]} 당신은</div> : <div>상대방은</div>}
         <div>귀염뽀짝 {myChar.name_ko} 과(와)</div>
-        <div>최고의 궁합!</div>
+        {encodedData ? (
+          <div>최고의 궁합!</div>
+        ) : (
+          <div>최고의 궁합이라고 하네요!</div>
+        )}
         <div className={styled.avgText}>
           (전체 참여자 중{" "}
           <span className={styled.avgSubText}>{myChar.avg}%</span> 가 같은
           주민이 나왔어요!)
         </div>
       </div>
-      <div className={styled.btnsWrapper}>
-        <CopyToClipboard text={value} onCopy={() => setCopied(true)}>
-          <button onClick={goToPosition}>{MatchButtonText.SHARE}</button>
-        </CopyToClipboard>
-        <button onClick={goToFirstPage}>{MatchButtonText.RETRY}</button>
-        <button onClick={goToPosition}>{MatchButtonText.TYPE}</button>
-        <button onClick={goToPosition}>{MatchButtonText.BEST}</button>
-        <button onClick={goToPosition}>{MatchButtonText.COMMENT}</button>
-      </div>
+      {encodedData && (
+        <>
+          <div className={styled.btnsWrapper}>
+            <CopyToClipboard text={value} onCopy={() => setCopied(true)}>
+              <button onClick={goToPosition}>{MatchButtonText.SHARE}</button>
+            </CopyToClipboard>
+            <button onClick={goToFirstPage}>{MatchButtonText.RETRY}</button>
+            <button onClick={goToPosition}>{MatchButtonText.TYPE}</button>
+            <button onClick={goToPosition}>{MatchButtonText.BEST}</button>
+            <button onClick={goToPosition}>{MatchButtonText.COMMENT}</button>
+          </div>
+        </>
+      )}
     </>
   );
 };
