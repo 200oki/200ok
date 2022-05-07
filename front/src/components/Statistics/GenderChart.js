b4fe555ff238d033290ed8e14bd5f67395f215d2
Jiwoo An <97489259+JiwooAn@users.noreply.github.com>
2022-05-07 19:50:05 +0900

    modify: add gender chart description

--- front/src/components/Statistics/GenderChart.js
+++ front/src/components/Statistics/GenderChart.js
@@ -2,11 +2,21 @@ import React, { useState, useEffect } from "react";
 import * as Api from "../../api";
 import { Chart, registerables } from "chart.js";
 import { Pie } from 'react-chartjs-2';
+import CustomModal from "../common/CustomModal";
+import { Typography } from "@mui/material";
+import { useStyles } from "../../utils/useStyles";
 Chart.register(...registerables)
 
 const SpeciesChart = () => {
   const [dataList, setDataList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isClicked, setIsClicked] = useState(false);
+  const classes = useStyles();
+
+  const handleModal = () => {
+    setIsClicked((v) => !v);
+  };
+  
   async function getDataList() {
     try {
       const { data } = await Api.get('stats?groupName=gender');
@@ -20,9 +30,28 @@ const SpeciesChart = () => {
   useEffect(() => {
     getDataList();
   }, []);
-  
+
   return (
-    <div>
+    <div className="graphDesc">
+      <div className={classes.desc} onClick={handleModal}>
+        ?
+      </div>
+      <CustomModal open={isClicked} onClose={handleModal}>
+        <Typography
+          id="modal-modal-description"
+          sx={{ mt: 2 }}
+          className={classes.modalFont}
+        >
+          동물의 숲에서 남녀 성비는 게이머가 보기에는 대체로 비슷한 편이지만
+        </Typography>
+        <Typography
+          id="modal-modal-description"
+          sx={{ mt: 2 }}
+          className={classes.modalFont}
+        >
+          성비를 실제로 계산해 보면 117.3으로 상당한 남초 마을입니다.
+        </Typography>
+      </CustomModal>
       <Pie className="graphBack"
         data={{
           labels: dataList[1],
